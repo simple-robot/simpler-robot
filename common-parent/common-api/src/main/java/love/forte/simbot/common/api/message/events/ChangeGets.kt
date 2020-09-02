6b802155a9dad4be9e523ab83a6d24addab94e4b
ForteScarlet <ForteScatlet@163.com>
2020-09-02 23:20:43 +0800

    changed events

--- common-parent/common-api/src/main/java/love/forte/simbot/common/api/message/events/ChangeGets.kt
+++ common-parent/common-api/src/main/java/love/forte/simbot/common/api/message/events/ChangeGets.kt
@@ -45,9 +45,6 @@ public interface GroupMemberPermissionChanged :
     override val beOperatorInfo: BeOperatorInfoContainer
         get() = AccountAsBeOperator(accountInfo)
 
-    // 得到管理权限
-    // 失去管理权限
-
     /**
      * 是否为 **得到管理权限**
      */
@@ -57,11 +54,77 @@ public interface GroupMemberPermissionChanged :
      * 是否为 **失去管理权限**
      */
     @JvmDefault val isLostManagementRights: Boolean get() = afterChange.isMember()
-
-
-
-
 }
+
+
+
+
+
+
+/**
+ * 群名称变动事件。
+ */
+@MainListenerType("群名称变更事件")
+public interface GroupNameChanged :
+    ChangedGet<String>,
+    GroupContainer,
+    OperatingContainer {
+    /**
+     * [被操作者][beOperatorInfo] 默认实现为 [当前主体账户][accountInfo]
+     */
+    @JvmDefault
+    override val beOperatorInfo: BeOperatorInfoContainer
+        get() = AccountAsBeOperator(accountInfo)
+}
+
+
+
+/**
+ * 群友群名片变动事件。
+ */
+@MainListenerType("群友群名片变动事件")
+public interface GroupMemberRemarkChanged :
+    ChangedGet<String>,
+    GroupContainer,
+    OperatingContainer {
+    /**
+     * [被操作者][beOperatorInfo] 默认实现为 [当前主体账户][accountInfo]
+     */
+    @JvmDefault
+    override val beOperatorInfo: BeOperatorInfoContainer
+        get() = AccountAsBeOperator(accountInfo)
+}
+
+
+/**
+ * 群友头衔变动事件。
+ */
+@MainListenerType("群友头衔变动事件")
+public interface GroupMemberSpecialChanged :
+    ChangedGet<String>,
+    GroupContainer,
+    OperatingContainer {
+    /**
+     * [被操作者][beOperatorInfo] 默认实现为 [当前主体账户][accountInfo]
+     */
+    @JvmDefault
+    override val beOperatorInfo: BeOperatorInfoContainer
+        get() = AccountAsBeOperator(accountInfo)
+}
+
+
+/**
+ * 好友昵称变动事件
+ */
+@MainListenerType("好友昵称变动事件")
+public interface FriendNicknameChanged : ChangedGet<String>
+
+
+/**
+ * 好友头像变动事件
+ */
+@MainListenerType("好友头像变动事件")
+public interface FriendAvatarChanged : ChangedGet<String>
 
 
 
