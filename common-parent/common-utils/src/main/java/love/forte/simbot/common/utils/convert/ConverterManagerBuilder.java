d3b5a7eeac0957bd75ec0b0c92519e251e3ad1b1
ForteScarlet <ForteScarlet@163.com>
2020-09-11 22:42:51 +0800

    类型转化器;

--- common-parent/common-utils/src/main/java/love/forte/simbot/common/utils/convert/ConverterManagerBuilder.java
+++ common-parent/common-utils/src/main/java/love/forte/simbot/common/utils/convert/ConverterManagerBuilder.java
@@ -10,7 +10,17 @@ import java.lang.reflect.Type;
  */
 public interface ConverterManagerBuilder {
 
+    /**
+     * 注册一个转化器
+     * @param target 目标类型
+     * @param converter 转化器
+     * @return 当前builder
+     */
     ConverterManagerBuilder register(Type target, Converter<?> converter);
 
+    /**
+     * 构建一个 {@link ConverterManager} 实例。
+     * @return {@link ConverterManager} 实例
+     */
     ConverterManager build();
 }
