d8c75fb3b83f1a833ba81e5c14558492cd01c38f
ForteScarlet <ForteScarlet@163.com>
2020-09-18 15:32:13 +0800

    AnnotationUtil

--- common-parent/utils-annotation/src/main/java/love/forte/common/utils/annotation/AnnotationInvocationHandler.java
+++ common-parent/utils-annotation/src/main/java/love/forte/common/utils/annotation/AnnotationInvocationHandler.java
@@ -1,15 +1,3 @@
-/*
- * Copyright (c) 2020. ForteScarlet All rights reserved.
- * Project  parent
- * File     AnnotationInvocationHandler.java
- *
- * You can contact the author through the following channels:
- * github https://github.com/ForteScarlet
- * gitee  https://gitee.com/ForteScarlet
- * email  ForteScarlet@163.com
- * QQ     1149159218
- */
-
 package love.forte.common.utils.annotation;
 
 import sun.reflect.annotation.ExceptionProxy;
@@ -68,6 +56,7 @@ public class AnnotationInvocationHandler implements InvocationHandler {
     public Object invoke(Object proxy, Method method, Object[] args) throws InvocationTargetException, IllegalAccessException {
         final String name = method.getName();
         Class<?>[] parameterTypes = method.getParameterTypes();
+        //noinspection AlibabaUndefineMagicConstant
         if ("equals".equals(name) && parameterTypes.length == 1 && parameterTypes[0] == Object.class) {
             return this.equalsImpl(args[0]);
         } else if (parameterTypes.length != 0) {
@@ -119,19 +108,17 @@ public class AnnotationInvocationHandler implements InvocationHandler {
             return false;
         } else {
             Method[] methods = this.getMemberMethods();
-            int methodLength = methods.length;
 
-            for (int i = 0; i < methodLength; ++i) {
-                Method var5 = methods[i];
-                String var6 = var5.getName();
-                Object var7 = this.memberValues.get(var6);
-                Object var8;
-                AnnotationInvocationHandler var9 = this.asOneOfUs(other);
-                if (var9 != null) {
-                    var8 = var9.memberValues.get(var6);
+            for (Method method : methods) {
+                String methodName = method.getName();
+                Object value = this.memberValues.get(methodName);
+                Object otherValue;
+                AnnotationInvocationHandler otherHandler = this.asOneOfUs(other);
+                if (otherHandler != null) {
+                    otherValue = otherHandler.memberValues.get(methodName);
                 } else {
                     try {
-                        var8 = var5.invoke(other);
+                        otherValue = method.invoke(other);
                     } catch (InvocationTargetException var11) {
                         return false;
                     } catch (IllegalAccessException var12) {
@@ -139,7 +126,7 @@ public class AnnotationInvocationHandler implements InvocationHandler {
                     }
                 }
 
-                if (!memberValueEquals(var7, var8)) {
+                if (!memberValueEquals(value, otherValue)) {
                     return false;
                 }
             }
@@ -148,11 +135,11 @@ public class AnnotationInvocationHandler implements InvocationHandler {
     }
 
 
-    private AnnotationInvocationHandler asOneOfUs(Object var1) {
-        if (Proxy.isProxyClass(var1.getClass())) {
-            InvocationHandler var2 = Proxy.getInvocationHandler(var1);
-            if (var2 instanceof AnnotationInvocationHandler) {
-                return (AnnotationInvocationHandler) var2;
+    private AnnotationInvocationHandler asOneOfUs(Object other) {
+        if (Proxy.isProxyClass(other.getClass())) {
+            InvocationHandler handler = Proxy.getInvocationHandler(other);
+            if (handler instanceof AnnotationInvocationHandler) {
+                return (AnnotationInvocationHandler) handler;
             }
         }
         return null;
@@ -161,10 +148,10 @@ public class AnnotationInvocationHandler implements InvocationHandler {
     private Method[] getMemberMethods() {
         if (this.memberMethods == null) {
             this.memberMethods = AccessController.doPrivileged((PrivilegedAction<Method[]>) () -> {
-                Method[] var1 = AnnotationInvocationHandler.this.type.getDeclaredMethods();
-                AnnotationInvocationHandler.this.validateAnnotationMethods(var1);
-                AccessibleObject.setAccessible(var1, true);
-                return var1;
+                Method[] methods = AnnotationInvocationHandler.this.type.getDeclaredMethods();
+                AnnotationInvocationHandler.this.validateAnnotationMethods(methods);
+                AccessibleObject.setAccessible(methods, true);
+                return methods;
             });
         }
 
@@ -189,12 +176,14 @@ public class AnnotationInvocationHandler implements InvocationHandler {
                     }
                 }
 
+                //noinspection AlibabaAvoidComplexCondition
                 if ((!returnType.isPrimitive() || returnType == Void.TYPE) && returnType != String.class && returnType != Class.class && !returnType.isEnum() && !returnType.isAnnotation()) {
                     notMalformed = false;
                     break;
                 }
 
                 String methodName = method.getName();
+                //noinspection AlibabaAvoidComplexCondition
                 if ((!"toString".equals(methodName) || returnType != String.class) && (!"hashCode".equals(methodName) || returnType != Integer.TYPE) && (!"annotationType".equals(methodName) || returnType != Class.class)) {
                     ++i;
                     continue;
@@ -215,57 +204,55 @@ public class AnnotationInvocationHandler implements InvocationHandler {
 
 
     private static boolean memberValueEquals(Object o1, Object o2) {
-        Class<?> var2 = o1.getClass();
-        if (!var2.isArray()) {
+        Class<?> o1Type = o1.getClass();
+        if (!o1Type.isArray()) {
             return o1.equals(o2);
         } else if (o1 instanceof Object[] && o2 instanceof Object[]) {
-            return Arrays.equals((Object[]) ((Object[]) o1), (Object[]) ((Object[]) o2));
-        } else if (o2.getClass() != var2) {
+            return Arrays.equals(((Object[]) o1), ((Object[]) o2));
+        } else if (o2.getClass() != o1Type) {
             return false;
-        } else if (var2 == byte[].class) {
-            return Arrays.equals((byte[]) ((byte[]) o1), (byte[]) ((byte[]) o2));
-        } else if (var2 == char[].class) {
-            return Arrays.equals((char[]) ((char[]) o1), (char[]) ((char[]) o2));
-        } else if (var2 == double[].class) {
-            return Arrays.equals((double[]) ((double[]) o1), (double[]) ((double[]) o2));
-        } else if (var2 == float[].class) {
-            return Arrays.equals((float[]) ((float[]) o1), (float[]) ((float[]) o2));
-        } else if (var2 == int[].class) {
-            return Arrays.equals((int[]) ((int[]) o1), (int[]) ((int[]) o2));
-        } else if (var2 == long[].class) {
-            return Arrays.equals((long[]) ((long[]) o1), (long[]) ((long[]) o2));
-        } else if (var2 == short[].class) {
-            return Arrays.equals((short[]) ((short[]) o1), (short[]) ((short[]) o2));
+        } else if (o1Type == byte[].class) {
+            return Arrays.equals((byte[]) o1, (byte[]) o2);
+        } else if (o1Type == char[].class) {
+            return Arrays.equals((char[]) o1, (char[]) o2);
+        } else if (o1Type == double[].class) {
+            return Arrays.equals((double[]) o1, (double[]) o2);
+        } else if (o1Type == float[].class) {
+            return Arrays.equals((float[]) o1, (float[]) o2);
+        } else if (o1Type == int[].class) {
+            return Arrays.equals((int[]) o1, (int[]) o2);
+        } else if (o1Type == long[].class) {
+            return Arrays.equals((long[]) o1, (long[]) o2);
+        } else if (o1Type == short[].class) {
+            return Arrays.equals((short[]) o1, (short[]) o2);
         } else {
-            assert var2 == boolean[].class;
+            assert o1Type == boolean[].class;
 
-            return Arrays.equals((boolean[]) ((boolean[]) o1), (boolean[]) ((boolean[]) o2));
+            return Arrays.equals((boolean[]) o1, (boolean[]) o2);
         }
     }
 
     private String toStringImpl() {
-        StringBuilder var1 = new StringBuilder(128);
-        var1.append('@');
-        var1.append(this.type.getName());
-        var1.append('(');
-        boolean var2 = true;
-        Iterator var3 = this.memberValues.entrySet().iterator();
+        StringBuilder builder = new StringBuilder(128);
+        builder.append('@');
+        builder.append(this.type.getName());
+        builder.append('(');
+        boolean first = true;
 
-        while (var3.hasNext()) {
-            Map.Entry var4 = (Map.Entry) var3.next();
-            if (var2) {
-                var2 = false;
+        for (Map.Entry<String, Object> entry : this.memberValues.entrySet()) {
+            if (first) {
+                first = false;
             } else {
-                var1.append(", ");
+                builder.append(", ");
             }
 
-            var1.append((String) var4.getKey());
-            var1.append('=');
-            var1.append(memberValueToString(var4.getValue()));
+            builder.append(entry.getKey());
+            builder.append('=');
+            builder.append(memberValueToString(entry.getValue()));
         }
 
-        var1.append(')');
-        return var1.toString();
+        builder.append(')');
+        return builder.toString();
     }
 
     private static String memberValueToString(Object var0) {
@@ -273,21 +260,21 @@ public class AnnotationInvocationHandler implements InvocationHandler {
         if (!var1.isArray()) {
             return var0.toString();
         } else if (var1 == byte[].class) {
-            return Arrays.toString((byte[]) ((byte[]) var0));
+            return Arrays.toString((byte[]) var0);
         } else if (var1 == char[].class) {
-            return Arrays.toString((char[]) ((char[]) var0));
+            return Arrays.toString((char[]) var0);
         } else if (var1 == double[].class) {
-            return Arrays.toString((double[]) ((double[]) var0));
+            return Arrays.toString((double[]) var0);
         } else if (var1 == float[].class) {
-            return Arrays.toString((float[]) ((float[]) var0));
+            return Arrays.toString((float[]) var0);
         } else if (var1 == int[].class) {
-            return Arrays.toString((int[]) ((int[]) var0));
+            return Arrays.toString((int[]) var0);
         } else if (var1 == long[].class) {
-            return Arrays.toString((long[]) ((long[]) var0));
+            return Arrays.toString((long[]) var0);
         } else if (var1 == short[].class) {
-            return Arrays.toString((short[]) ((short[]) var0));
+            return Arrays.toString((short[]) var0);
         } else {
-            return var1 == boolean[].class ? Arrays.toString((boolean[]) ((boolean[]) var0)) : Arrays.toString((Object[]) ((Object[]) var0));
+            return var1 == boolean[].class ? Arrays.toString((boolean[]) var0) : Arrays.toString((Object[]) var0);
         }
     }
 
@@ -295,31 +282,31 @@ public class AnnotationInvocationHandler implements InvocationHandler {
     private Object cloneArray(Object var1) {
         Class<?> var2 = var1.getClass();
         if (var2 == byte[].class) {
-            byte[] var6 = (byte[]) ((byte[]) var1);
+            byte[] var6 = (byte[]) var1;
             return var6.clone();
         } else if (var2 == char[].class) {
-            char[] var5 = (char[]) ((char[]) var1);
+            char[] var5 = (char[]) var1;
             return var5.clone();
         } else if (var2 == double[].class) {
-            double[] var4 = (double[]) ((double[]) var1);
+            double[] var4 = (double[]) var1;
             return var4.clone();
         } else if (var2 == float[].class) {
-            float[] var11 = (float[]) ((float[]) var1);
+            float[] var11 = (float[]) var1;
             return var11.clone();
         } else if (var2 == int[].class) {
-            int[] var10 = (int[]) ((int[]) var1);
+            int[] var10 = (int[]) var1;
             return var10.clone();
         } else if (var2 == long[].class) {
-            long[] var9 = (long[]) ((long[]) var1);
+            long[] var9 = (long[]) var1;
             return var9.clone();
         } else if (var2 == short[].class) {
-            short[] var8 = (short[]) ((short[]) var1);
+            short[] var8 = (short[]) var1;
             return var8.clone();
         } else if (var2 == boolean[].class) {
-            boolean[] var7 = (boolean[]) ((boolean[]) var1);
+            boolean[] var7 = (boolean[]) var1;
             return var7.clone();
         } else {
-            Object[] var3 = (Object[]) ((Object[]) var1);
+            Object[] var3 = (Object[]) var1;
             return var3.clone();
         }
     }
@@ -327,9 +314,9 @@ public class AnnotationInvocationHandler implements InvocationHandler {
     private int hashCodeImpl() {
         int var1 = 0;
 
-        Map.Entry var3;
-        for (Iterator var2 = this.memberValues.entrySet().iterator(); var2.hasNext(); var1 += 127 * ((String) var3.getKey()).hashCode() ^ memberValueHashCode(var3.getValue())) {
-            var3 = (Map.Entry) var2.next();
+        Map.Entry<String, Object> var3;
+        for (Iterator<Map.Entry<String, Object>> var2 = this.memberValues.entrySet().iterator(); var2.hasNext(); var1 += 127 * var3.getKey().hashCode() ^ memberValueHashCode(var3.getValue())) {
+            var3 = var2.next();
         }
 
         return var1;
@@ -340,21 +327,21 @@ public class AnnotationInvocationHandler implements InvocationHandler {
         if (!var1.isArray()) {
             return var0.hashCode();
         } else if (var1 == byte[].class) {
-            return Arrays.hashCode((byte[]) ((byte[]) var0));
+            return Arrays.hashCode((byte[]) var0);
         } else if (var1 == char[].class) {
-            return Arrays.hashCode((char[]) ((char[]) var0));
+            return Arrays.hashCode((char[]) var0);
         } else if (var1 == double[].class) {
-            return Arrays.hashCode((double[]) ((double[]) var0));
+            return Arrays.hashCode((double[]) var0);
         } else if (var1 == float[].class) {
-            return Arrays.hashCode((float[]) ((float[]) var0));
+            return Arrays.hashCode((float[]) var0);
         } else if (var1 == int[].class) {
-            return Arrays.hashCode((int[]) ((int[]) var0));
+            return Arrays.hashCode((int[]) var0);
         } else if (var1 == long[].class) {
-            return Arrays.hashCode((long[]) ((long[]) var0));
+            return Arrays.hashCode((long[]) var0);
         } else if (var1 == short[].class) {
-            return Arrays.hashCode((short[]) ((short[]) var0));
+            return Arrays.hashCode((short[]) var0);
         } else {
-            return var1 == boolean[].class ? Arrays.hashCode((boolean[]) ((boolean[]) var0)) : Arrays.hashCode((Object[]) ((Object[]) var0));
+            return var1 == boolean[].class ? Arrays.hashCode((boolean[]) var0) : Arrays.hashCode((Object[]) var0);
         }
     }
 }
