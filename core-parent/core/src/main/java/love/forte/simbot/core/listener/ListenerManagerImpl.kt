51a06716209a659b245f48f20958feb512190cfa
ForteScarlet <ForteScarlet@163.com>
2020-10-07 18:29:20 +0800

    listener manager.

--- core-parent/core/src/main/java/love/forte/simbot/core/listener/ListenerManagerImpl.kt
+++ core-parent/core/src/main/java/love/forte/simbot/core/listener/ListenerManagerImpl.kt
@@ -12,9 +12,9 @@
 
 package love.forte.simbot.core.listener
 
+import love.forte.common.collections.concurrentSortedQueueOf
 import love.forte.simbot.core.api.message.MsgGet
 import love.forte.simbot.core.filter.AtDetectionFactory
-import love.forte.simbot.core.filter.FilterManager
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
@@ -22,6 +22,11 @@ import java.util.concurrent.ConcurrentHashMap
  * 储存监听函数的List。使用有序队列。
  */
 private fun managerList(): MutableCollection<ListenerFunction> = PriorityQueue(ListenerFunctionComparable)
+
+/**
+ * 监听函数自排序队列。
+ */
+private fun listenerFunctionQueue(vararg func: ListenerFunction): Queue<ListenerFunction> = concurrentSortedQueueOf(ListenerFunctionComparable , *func)
 
 
 /**
@@ -33,14 +38,24 @@ public class ListenerManagerImpl(
 
     /**
      * 监听函数集合，通过对应的监听类型进行分类。
+     *
+     * 此为主集合，所有的监听函数均存在于此。
      */
-    private val listenerFunctionMap: MutableMap<Class<out MsgGet>, MutableCollection<ListenerFunction>> =
+    private val mainListenerFunctionMap: MutableMap<Class<out MsgGet>, Queue<ListenerFunction>> =
         ConcurrentHashMap()
+
+    /**
+     * 监听函数缓冲区，对后续出现的消息类型进行记录并缓存。
+     * 当 [register] 了新的监听函数后对应相关类型将会被清理。
+     */
+    private val cacheListenerFunctionMap: MutableMap<Class<out MsgGet>, Queue<ListenerFunction>> =
+        ConcurrentHashMap()
+
 
     /**
      * 目前所有监听中的类型。
      */
-    private val allTypes: MutableSet<Class<out MsgGet>> get() = listenerFunctionMap.keys
+    private val allListenTypes: MutableSet<Class<out MsgGet>> get() = mainListenerFunctionMap.keys
 
     /**
      * 注册一个 [监听函数][ListenerFunction]。
@@ -49,10 +64,19 @@ public class ListenerManagerImpl(
         // 获取其监听类型，并作为key存入map
         val listenType = listenerFunction.listenType
 
-        // merge.
-        listenerFunctionMap.merge(listenType, managerList()) { oldValue, value ->
+        // merge into map.
+        mainListenerFunctionMap.merge(listenType, listenerFunctionQueue(listenerFunction)) { oldValue, value ->
             oldValue.apply { addAll(value) }
         }
+
+        // clear cache map.
+        // 寻找并更新缓存监听
+        cacheListenerFunctionMap.forEach {
+            if(listenType.isAssignableFrom(it.key)) {
+                it.value.add(listenerFunction)
+            }
+        }
+
 
     }
 
