eb21771c781accda10704a248b8f917e33bd4477
ForteScarlet <ForteScarlet@163.com>
2020-10-16 00:00:52 +0800

    logger conf.

--- component-parent/component-mirai/src/main/java/love/forte/simbot/component/mirai/SimbotMiraiLogger.kt
+++ component-parent/component-mirai/src/main/java/love/forte/simbot/component/mirai/SimbotMiraiLogger.kt
@@ -20,6 +20,7 @@ package love.forte.simbot.component.mirai
 import net.mamoe.mirai.utils.MiraiLoggerPlatformBase
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import sun.reflect.CallerSensitive
 
 /**
  * 默认使用的普通日志实现，不会区分bot
@@ -28,22 +29,27 @@ object SimbotMiraiLogger : MiraiLoggerPlatformBase() {
     override val identity: String = "Simbot-Mirai"
     private val logger: Logger = LoggerFactory.getLogger(identity)
 
+    @CallerSensitive
     override fun debug0(message: String?, e: Throwable?) {
         e?.let { logger.debug(message, it) } ?: logger.debug("{}", message)
     }
 
+    @CallerSensitive
     override fun error0(message: String?, e: Throwable?) {
         e?.let { logger.error(message, it) } ?: logger.error("{}", message)
     }
 
+    @CallerSensitive
     override fun info0(message: String?, e: Throwable?) {
         e?.let { logger.info(message, it) } ?: logger.info("{}", message)
     }
 
+    @CallerSensitive
     override fun verbose0(message: String?, e: Throwable?) {
         e?.let { logger.trace(message, it) } ?: logger.trace("{}", message)
     }
 
+    @CallerSensitive
     override fun warning0(message: String?, e: Throwable?) {
         e?.let { logger.warn(message, it) } ?: logger.warn("{}", message)
     }
