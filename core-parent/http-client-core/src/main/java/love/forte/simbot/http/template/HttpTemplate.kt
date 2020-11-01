5e6e9443f5165e61c81594a62da2889767153e90
ForteScarlet <ForteScarlet@163.com>
2020-11-01 20:38:02 +0800

    http client;

---
+++ core-parent/http-client-core/src/main/java/love/forte/simbot/http/template/HttpTemplate.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2020. ForteScarlet All rights reserved.
+ * Project  parent
+ * File     HttpTemplate.kt
+ *
+ * You can contact the author through the following channels:
+ * github https://github.com/ForteScarlet
+ * gitee  https://gitee.com/ForteScarlet
+ * email  ForteScarlet@163.com
+ * QQ     1149159218
+ */
+
+package love.forte.simbot.http.template
+
+
+/**
+ * 简易的 http 客户端。
+ * @author ForteScarlet -> https://github.com/ForteScarlet
+ */
+public interface HttpTemplate {
+}
