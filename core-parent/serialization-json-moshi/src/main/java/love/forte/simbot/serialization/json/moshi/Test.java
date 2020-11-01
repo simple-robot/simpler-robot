37c7ca80c74e96fe1f412a880a5fcad618a00988
ForteScarlet <ForteScarlet@163.com>
2020-11-02 00:35:59 +0800

    json-moshi

---
+++ core-parent/serialization-json-moshi/src/main/java/love/forte/simbot/serialization/json/moshi/Test.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020. ForteScarlet All rights reserved.
+ * Project  parent
+ * File     Test.java
+ *
+ * You can contact the author through the following channels:
+ * github https://github.com/ForteScarlet
+ * gitee  https://gitee.com/ForteScarlet
+ * email  ForteScarlet@163.com
+ * QQ     1149159218
+ */
+
+package love.forte.simbot.serialization.json.moshi;
+
+import com.squareup.moshi.Moshi;
+
+import java.io.IOException;
+
+/**
+ * @author <a href="https://github.com/ForteScarlet"> ForteScarlet </a>
+ */
+public class Test {
+    public static void main(String[] args) throws IOException {
+        final Moshi moshi = new Moshi.Builder().build();
+
+        String jsonStr = "{\"username\":\"jack\",\"password\":\"123\"}";
+
+        final User user = moshi.adapter(User.class).fromJson(jsonStr);
+
+        System.out.println(user);
+
+    }
+}
---
+++ core-parent/serialization-json-moshi/src/main/java/love/forte/simbot/serialization/json/moshi/User.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020. ForteScarlet All rights reserved.
+ * Project  parent
+ * File     User.java
+ *
+ * You can contact the author through the following channels:
+ * github https://github.com/ForteScarlet
+ * gitee  https://gitee.com/ForteScarlet
+ * email  ForteScarlet@163.com
+ * QQ     1149159218
+ */
+
+package love.forte.simbot.serialization.json.moshi;
+
+/**
+ * @author <a href="https://github.com/ForteScarlet"> ForteScarlet </a>
+ */
+public class User {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
