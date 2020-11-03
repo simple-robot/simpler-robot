95901e6cce3159051690f9a8237c32f5fece4fa7
ForteScarlet <ForteScarlet@163.com>
2020-11-03 20:05:26 +0800

    http-template-core

---
+++ core-parent/http-client-core/src/main/java/love/forte/simbot/http/template/HttpHeaders.kt
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2020. ForteScarlet All rights reserved.
+ * Project  parent
+ * File     HttpHeaders.kt
+ *
+ * You can contact the author through the following channels:
+ * github https://github.com/ForteScarlet
+ * gitee  https://gitee.com/ForteScarlet
+ * email  ForteScarlet@163.com
+ * QQ     1149159218
+ */
+
+package love.forte.simbot.http.template
+
+/**
+ *
+ * http 请求头类型，可用于请求或者接受相应。
+ *
+ * 请求头类似于键值对格式，但是key对应的value是可以存在多个的，
+ * 因此其为一个 `Map<String, List<String>>` 。
+ *
+ *
+ */
+public interface HttpHeaders : MutableMap<String, MutableList<String>> {
+
+    /**
+     * 获取请求头多个值中的第一个。
+     * @return 请求头的第一个值。如果没有此请求头则返回null。
+     */
+    fun getFirst(headerKey: String): String?
+
+    /**
+     * 获取请求头的指定索引的值。
+     * @return 请求头中的指定值。如果没有此请求头则返回null。
+     * @throws IndexOutOfBoundsException 如果存在请求头，但是索引越界了
+     */
+    fun getIndexed(headerKey: String, index: Int): String?
+
+    /**
+     * 添加一个值。
+     */
+    fun add(header: String, value: String)
+
+
+    /**
+     * 添加多个值。
+     */
+    fun addMultiple(header: String, headerValues: Collection<String>)
+
+
+    /**
+     * 设置一个值。会覆盖原有的值。等同于put，但是set的值是一个单个的值。
+     */
+    fun set(header: String, value: String)
+
+
+
+    /** 提供部分默认实现。 */
+    companion object {
+
+        /**
+         * 获取一个内容为空的基础实例。
+         */
+        val instance: HttpHeaders get() = MapHttpHeaders()
+
+        /**
+         * 根据单键值对map构建 [HttpHeaders] 实例。
+         */
+        fun fromSingleValueMap(map: Map<String, String>): HttpHeaders {
+            return MapHttpHeaders(map.mapValues { mutableListOf(it.value) }.toMutableMap())
+        }
+
+        /**
+         * 根据多值键值对map构建 [HttpHeaders] 实例。
+         */
+        fun fromMultiValueMap(map: Map<String, List<String>>): HttpHeaders {
+            return MapHttpHeaders(map.mapValues { it.value.toMutableList() }.toMutableMap())
+        }
+    }
+
+
+}
+
+
+/**
+ * 基于 [Map] 实现的 [HttpHeaders].
+ */
+private class MapHttpHeaders(private val map: MutableMap<String, MutableList<String>> = mutableMapOf()) : HttpHeaders, MutableMap<String, MutableList<String>> by map {
+
+    /**
+     * 获取第一个。
+     */
+    override fun getFirst(headerKey: String): String? = getIndexed(headerKey, 0)
+
+    /**
+     * 指定索引。
+     */
+    override fun getIndexed(headerKey: String, index: Int): String? =
+        map[headerKey]?.takeIf { it.isNotEmpty() }?.get(index)
+
+    /**
+     * 添加一个单个的值。
+     */
+    override fun add(header: String, value: String) {
+        map.compute(header) {_, old ->
+            old?.apply { add(value) } ?: mutableListOf(value)
+        }
+    }
+
+    /**
+     * 添加多个。
+     */
+    override fun addMultiple(header: String, headerValues: Collection<String>) {
+        map.compute(header) { _, old ->
+            val valueList: MutableList<String> = headerValues.toMutableList()
+            if (old != null) {
+                valueList.addAll(old)
+            }
+            valueList
+        }
+    }
+
+    /**
+     * 覆盖一个值。
+     */
+    override fun set(header: String, value: String) {
+        put(header, mutableListOf(value))
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- core-parent/http-client-core/src/main/java/love/forte/simbot/http/template/HttpTemplate.kt
+++ core-parent/http-client-core/src/main/java/love/forte/simbot/http/template/HttpTemplate.kt
@@ -10,12 +10,162 @@
  * QQ     1149159218
  */
 
+@file:JvmName("HttpTemplateUtil")
 package love.forte.simbot.http.template
 
 
 /**
- * 简易的 http 客户端。
+ * 简易的 http 客户端，提供部分最常见的一些请求方式。
  * @author ForteScarlet -> https://github.com/ForteScarlet
  */
 public interface HttpTemplate {
+
+    /**
+     * get请求。
+     * @param responseType 响应body封装类型。
+     */
+    fun <T> get(url: String, responseType: Class<T>): HttpResponse<T>
+
+    /**
+     * get请求。
+     * @param responseType 响应body封装类型。
+     * @param headers 请求头信息。
+     */
+    fun <T> get(url: String, headers: HttpHeaders?, responseType: Class<T>): HttpResponse<T>
+
+    /**
+     * get请求。
+     * @param responseType 响应body封装类型。
+     * @param headers 请求头信息。
+     * @param requestParam 请求参数。
+     */
+    fun <T> get(url: String, headers: HttpHeaders?, requestParam: Map<String, Any>?, responseType: Class<T>): HttpResponse<T>
+
+    /**
+     * post/json 请求。
+     * @param responseType 响应body封装类型。
+     */
+    fun <T> postJson(url: String, responseType: Class<T>): HttpResponse<T>
+
+    /**
+     * post/json 请求。
+     * @param responseType 响应body封装类型。
+     * @param headers 请求头信息。
+     */
+    fun <T> postJson(url: String, headers: HttpHeaders?, responseType: Class<T>): HttpResponse<T>
+
+    /**
+     * post/json 请求。
+     * @param responseType 响应body封装类型。
+     * @param headers 请求头信息。
+     * @param requestBody 请求参数，一个对象实例，或者一个json字符串。
+     */
+    fun <T> postJson(url: String, headers: HttpHeaders?, requestBody: Any?, responseType: Class<T>): HttpResponse<T>
+
+    /**
+     * post/form 请求。
+     * @param responseType 响应body封装类型。
+     */
+    fun <T> postForm(url: String, responseType: Class<T>): HttpResponse<T>
+
+    /**
+     * post/form 请求。
+     * @param responseType 响应body封装类型。
+     * @param headers 请求头信息。
+     */
+    fun <T> postForm(url: String, headers: HttpHeaders?, responseType: Class<T>): HttpResponse<T>
+
+    // /**
+    //  * post/form 请求。
+    //  * @param responseType 响应body封装类型。
+    //  * @param headers 请求头信息。
+    //  * @param requestForm 请求参数，一个对象实例，此对象实例只会获取其中一层字段值作为表单提交，不会像json那样嵌套获取。如果字段对应的是一个其他的实例，则会直接获取其toString的值。
+    //  */
+    // fun <T> postForm(url: String, headers: HttpHeaders?, requestForm: Any?, responseType: Class<T>): HttpResponse<T>
+
+    /**
+     * post/form 请求。
+     * @param responseType 响应body封装类型。
+     * @param headers 请求头信息。
+     * @param requestForm 请求参数，一个对象实例，此对象实例只会获取其中一层字段值作为表单提交，不会像json那样嵌套获取。如果字段对应的是一个其他的实例，则会直接获取其toString的值。
+     */
+    fun <T> postForm(url: String, headers: HttpHeaders?, requestForm: Map<String, Any>?, responseType: Class<T>): HttpResponse<T>
+
 }
+
+
+/**
+ * http 的请求相应简易模型。
+ */
+public interface HttpResponse<T> {
+    /**
+     * 响应状态码
+     */
+    val statusCode: Int
+
+
+    /**
+     * 响应体
+     */
+    val body: T
+
+
+    /**
+     * 获取响应的请求头信息。
+     */
+    val headers: HttpHeaders
+
+
+    /**
+     * 如果响应出现错误等情况，则此处可能为响应的错误信息。
+     */
+    val message: String?
+}
+
+//
+// /**
+//  * 将一个实例对象解析为单层提交表单。
+//  */
+// public fun Any.toRequestForm(): Map<String, Any> {
+//     return when(this) {
+//         is String, is Number, is Char, is Boolean -> mapOf("" to this)
+//
+//         is Date -> mapOf("" to time)
+//         is Instant -> mapOf("" to this.toEpochMilli())
+//         is Map<*, *> -> this.asSequence()
+//             .filter { it.key != null && it.value != null }
+//             .map { it.key.toString() to it.value.toString() }
+//             .toMap()
+//         is Pair<*, *> -> if (first != null && second != null) mapOf(first.toString() to second.toString()) else emptyMap()
+//         is Map.Entry<*, *> -> if (key != null && value != null) mapOf(key.toString() to value.toString()) else emptyMap()
+//         is Collection<*> -> mapOf("" to this)
+//         else -> {
+//             if ()
+//             this::class.members
+//
+//
+//             TODO()
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
