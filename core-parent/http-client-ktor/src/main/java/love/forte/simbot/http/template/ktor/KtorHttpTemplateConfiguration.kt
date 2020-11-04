8b226e86206b1dfbd1e4e6465c7137ef773f5640
ForteScarlet <ForteScatlet@163.com>
2020-11-04 17:34:44 +0800

    http template.

---
+++ core-parent/http-client-ktor/src/main/java/love/forte/simbot/http/template/ktor/KtorHttpTemplateConfiguration.kt
@@ -0,0 +1,37 @@
+/*
+ *
+ *  * Copyright (c) 2020. ForteScarlet All rights reserved.
+ *  * Project  simple-robot-S
+ *  * File     KtorHttpTemplateConfiguration.kt
+ *  *
+ *  * You can contact the author through the following channels:
+ *  * github https://github.com/ForteScarlet
+ *  * gitee  https://gitee.com/ForteScarlet
+ *  * email  ForteScarlet@163.com
+ *  * QQ     1149159218
+ *  *
+ *  *
+ *
+ */
+
+package love.forte.simbot.http.template.ktor
+
+import love.forte.common.ioc.annotation.Beans
+import love.forte.common.ioc.annotation.ConfigBeans
+import love.forte.simbot.serialization.json.JsonSerializerFactory
+
+/**
+ *
+ *
+ * @author ForteScarlet <ForteScarlet@163.com>
+ * @date 2020/11/4
+ * @since
+ */
+@ConfigBeans
+public class KtorHttpTemplateConfiguration {
+
+    @Beans("ktorHttpTemplate")
+    public fun ktorHttpTemplate(jsonSerializerFactory: JsonSerializerFactory) =
+        KtorHttpTemplate(jsonSerializerFactory = jsonSerializerFactory)
+
+}
