d4dc14d98d90b223f06bed02ea2289f2c9acfb0c
ForteScarlet <ForteScatlet@163.com>
2020-11-19 10:09:15 +0800

    logger.

--- core-parent/core/src/main/java/love/forte/simbot/core/_Logger.kt
+++ core-parent/core/src/main/java/love/forte/simbot/core/_Logger.kt
@@ -12,8 +12,12 @@
  *
  */
 
+@file:Suppress("NOTHING_TO_INLINE")
+@file:JvmName("CoreLoggerHelper")
+
 package love.forte.simbot.core
 
+import love.forte.common.language.Language
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -27,7 +31,38 @@ public abstract class TypedCompLogger(type: Class<*>) {
 }
 
 
+public inline fun Logger.infof(target: String, vararg args: Any?) {
+    info(Language.format(target, args))
+}
+public inline fun Logger.debugf(target: String, vararg args: Any?) {
+    debug(Language.format(target, args))
+}
+public inline fun Logger.warnf(target: String, vararg args: Any?) {
+    warn(Language.format(target, args))
+}
+public inline fun Logger.errorf(target: String, vararg args: Any?) {
+    error(Language.format(target, args))
+}
+public inline fun Logger.tracef(target: String, vararg args: Any?) {
+    trace(Language.format(target, args))
+}
 
+
+public inline fun Logger.infoEf(target: String, e: Throwable, vararg args: Any?) {
+    info(Language.format(target, args), e)
+}
+public inline fun Logger.debugEf(target: String, e: Throwable, vararg args: Any?) {
+    debug(Language.format(target, args), e)
+}
+public inline fun Logger.warnEf(target: String, e: Throwable, vararg args: Any?) {
+    warn(Language.format(target, args), e)
+}
+public inline fun Logger.errorEf(target: String, e: Throwable, vararg args: Any?) {
+    error(Language.format(target, args), e)
+}
+public inline fun Logger.traceEf(target: String, e: Throwable, vararg args: Any?) {
+    trace(Language.format(target, args), e)
+}
 
 
 
