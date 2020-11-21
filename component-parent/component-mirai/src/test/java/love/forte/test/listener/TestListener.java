258d6772352f2957525dccfde12a4caf785527b8
ForteScarlet <ForteScarlet@163.com>
2020-11-21 23:45:05 +0800

    update test;

--- component-parent/component-mirai/src/test/java/love/forte/test/listener/TestListener.java
+++ component-parent/component-mirai/src/test/java/love/forte/test/listener/TestListener.java
@@ -1,22 +1,40 @@
 package love.forte.test.listener;
 
 import love.forte.common.ioc.annotation.Beans;
+import love.forte.simbot.annotation.Filter;
+import love.forte.simbot.annotation.OnGroup;
 import love.forte.simbot.annotation.OnPrivate;
 import love.forte.simbot.api.message.events.PrivateMsg;
+import love.forte.simbot.filter.MatchType;
 
 @Beans
 public class TestListener {
 
+    //
+    // @OnPrivate
+    // @Filter(value = "hi", matchType = MatchType.STARTS_WITH)
+    // @Filter(value = "hello", matchType = MatchType.STARTS_WITH)
+    // public void l1(PrivateMsg msg){
+    //     System.out.println("hi or hello.");
+    // }
+    // @OnPrivate
+    // @Filter(value = "mua", matchType = MatchType.STARTS_WITH)
+    // @Filter(value = "qaq", matchType = MatchType.STARTS_WITH)
+    // public void l2(PrivateMsg msg){
+    //     System.out.println("mua or qaq.");
+    // }
 
-    @OnPrivate
-    public void listenQuote(PrivateMsg msg){
-        long n1 = System.nanoTime();
-        System.out.println(msg.getText());
-        long n2 = System.nanoTime();
-        System.out.println(msg.getMsg());
-        long n3 = System.nanoTime();
-        System.out.println("text time:\t" + /*TimeUnit.NANOSECONDS.toMillis*/(n2 - n1));
-        System.out.println("msg time:\t" + /*TimeUnit.NANOSECONDS.toMillis*/(n3 - n2));
+    @OnGroup
+    @Filter(value = "hi", matchType = MatchType.STARTS_WITH)
+    @Filter(value = "hello", matchType = MatchType.STARTS_WITH)
+    public void l1(){
+        System.out.println("hi or hello.");
+    }
+    @OnGroup
+    @Filter(value = "mua", matchType = MatchType.STARTS_WITH)
+    @Filter(value = "qaq", matchType = MatchType.STARTS_WITH)
+    public void l2(){
+        System.out.println("mua or qaq.");
     }
 
 
