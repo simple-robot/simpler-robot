f1eec568edf755876e858f82a1a79a47da4a61a0
ForteScarlet <fortescarlet@163.com>
2021-01-15 17:33:20 +0800

    能力接口

---
+++ core-parent/api/src/main/java/love/forte/simbot/api/message/ability/Ability.kt
@@ -0,0 +1,24 @@
+/*
+ *
+ *  * Copyright (c) 2020. ForteScarlet All rights reserved.
+ *  * Project  component-onebot
+ *  * File     Ability.kt
+ *  *
+ *  * You can contact the author through the following channels:
+ *  * github https://github.com/ForteScarlet
+ *  * gitee  https://gitee.com/ForteScarlet
+ *  * email  ForteScarlet@163.com
+ *  * QQ     1149159218
+ *  *
+ *  *
+ *
+ */
+
+package love.forte.simbot.api.message.ability
+
+
+/**
+ * 一个 **能力** 的父接口。
+ */
+public interface Ability {
+}
