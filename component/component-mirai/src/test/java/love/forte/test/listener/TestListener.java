96c1ee6301e7f1e3f824c83c04d23b5e0b1fc730
ForteScarlet <ForteScarlet@163.com>
2021-01-16 00:08:07 +0800

    test

--- component/component-mirai/src/test/java/love/forte/test/listener/TestListener.java
+++ component/component-mirai/src/test/java/love/forte/test/listener/TestListener.java
@@ -16,40 +16,25 @@ package love.forte.test.listener;
 
 import love.forte.common.ioc.annotation.Beans;
 import love.forte.simbot.annotation.ListenBreak;
-import love.forte.simbot.annotation.OnPrivate;
-import love.forte.simbot.annotation.SpareListen;
-import love.forte.simbot.api.message.containers.AccountInfo;
-import love.forte.simbot.api.message.events.PrivateMsg;
+import love.forte.simbot.annotation.OnGroup;
+import love.forte.simbot.api.message.events.GroupMsg;
 
 /**
  * @author ForteScarlet
  */
 @Beans
-@OnPrivate
+@OnGroup
 public class TestListener {
 
 
 
     @ListenBreak
-    public void lis1(PrivateMsg msg) {
-        System.out.println("lis1.");
-        AccountInfo accountInfo = msg.getAccountInfo();
-        System.out.println(accountInfo.getAccountNickname());
+    public void lis1(GroupMsg msg) {
+        System.out.println(msg);
+        System.out.println(msg.getText());
+        System.out.println(msg.getMsg());
     }
 
 
-    public boolean lis2(PrivateMsg msg) {
-        System.out.println("lis2.");
-        AccountInfo accountInfo = msg.getAccountInfo();
-        System.out.println(accountInfo.getAccountNickname() + ": " + msg.getText());
-        return msg.getText().equals("123");
-    }
-
-    @SpareListen
-    public void lis3(PrivateMsg msg) {
-        System.out.println("spare!");
-        AccountInfo accountInfo = msg.getAccountInfo();
-        System.out.println(accountInfo.getAccountNickname());
-    }
 
 }
