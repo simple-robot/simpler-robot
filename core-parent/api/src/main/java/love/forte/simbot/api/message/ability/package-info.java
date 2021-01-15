39bdf2a3c3915ef2001b88fdc2328c546ba07ab7
ForteScarlet <fortescarlet@163.com>
2021-01-15 17:33:11 +0800

    package info

---
+++ core-parent/api/src/main/java/love/forte/simbot/api/message/ability/package-info.java
@@ -0,0 +1,22 @@
+
+/*
+ *
+ *  * Copyright (c) 2020. ForteScarlet All rights reserved.
+ *  * Project  component-onebot
+ *  * File     package-info.java
+ *  *
+ *  * You can contact the author through the following channels:
+ *  * github https://github.com/ForteScarlet
+ *  * gitee  https://gitee.com/ForteScarlet
+ *  * email  ForteScarlet@163.com
+ *  * QQ     1149159218
+ *  *
+ *  *
+ *
+ */
+
+/**
+ * ability: 能力。
+ * 此包下的接口定义一些可能被赋予的能力，例如允许进行 <strong>决策</strong> 的能力，比如决策：通过或拒绝。
+ */
+package love.forte.simbot.api.message.ability;
