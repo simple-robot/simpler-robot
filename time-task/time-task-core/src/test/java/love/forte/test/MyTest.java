ff03db7734b30994ce53a45243b4f0f5a3b83868
ForteScarlet <fortescarlet@163.com>
2021-01-16 15:33:27 +0800

    test

---
+++ time-task/time-task-core/src/test/java/love/forte/test/MyTest.java
@@ -0,0 +1,41 @@
+/*
+ *
+ *  * Copyright (c) 2020. ForteScarlet All rights reserved.
+ *  * Project  component-onebot
+ *  * File     MyTest.java
+ *  *
+ *  * You can contact the author through the following channels:
+ *  * github https://github.com/ForteScarlet
+ *  * gitee  https://gitee.com/ForteScarlet
+ *  * email  ForteScarlet@163.com
+ *  * QQ     1149159218
+ *  *
+ *  *
+ *
+ */
+
+package love.forte.test;
+
+import love.forte.common.ioc.annotation.Beans;
+import love.forte.simbot.timer.EnableTimeTask;
+import love.forte.simbot.timer.Fixed;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author ForteScarlet
+ */
+@Beans
+@EnableTimeTask
+public class MyTest {
+
+
+    /**
+     * 5分钟执行一次。
+     */
+    @Fixed(value = 5, timeUnit = TimeUnit.MINUTES)
+    public void task() {
+        // do your task...
+    }
+
+}
