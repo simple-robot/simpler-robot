3544c9ec8aa107c1a3b970b9105bd826b0c1256d
ForteScarlet <fortescarlet@163.com>
2021-01-21 11:05:54 +0800

    test

--- component/component-mirai/src/test/java/love/forte/test/listener/TestListener.kt
+++ component/component-mirai/src/test/java/love/forte/test/listener/TestListener.kt
@@ -15,13 +15,9 @@
 package love.forte.test.listener
 
 import love.forte.common.ioc.annotation.Beans
-import love.forte.simbot.annotation.Filters
-import love.forte.simbot.annotation.Listen
-import love.forte.simbot.annotation.Listens
-import love.forte.simbot.api.message.Reply
-import love.forte.simbot.api.message.events.GroupMsg
-import love.forte.simbot.api.message.events.PrivateMsg
-import love.forte.simbot.component.mirai.message.event.MiraiPrivateMsg
+import love.forte.simbot.annotation.OnGroup
+import love.forte.simbot.annotation.OnPrivate
+import love.forte.simbot.api.message.events.MsgGet
 
 /**
  * @author ForteScarlet
@@ -29,18 +25,11 @@ import love.forte.simbot.component.mirai.message.event.MiraiPrivateMsg
 @Beans
 class TestListener {
 
-    /**
-     * 监听 [私聊消息][PrivateMsg]与[群聊消息][GroupMsg],
-     * 并在收到消息的时候统一回复 "Hello. this is Quick reply.", 如果在群聊中，会AT触发事件的人。
-     * 其中，在群聊中需要被AT。
-     */
-    @Listens(
-        value = [
-            Listen(PrivateMsg::class),
-            Listen(GroupMsg::class),
-        ]
-    )
-    @Filters(atBot = true)
-    fun listen(msg: MiraiPrivateMsg) = Reply.reply("Hello. this is Quick reply.", at = true)
+    @OnPrivate
+    @OnGroup
+    fun listen(msg: MsgGet) {
+        println(msg)
+
+    }// = Reply.reply("Hello. this is Quick reply.", at = true)
 }
 
