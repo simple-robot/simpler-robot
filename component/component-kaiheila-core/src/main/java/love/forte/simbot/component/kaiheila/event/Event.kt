6b92462cc92be87edad79586dc302cb20f10f69e
ForteScarlet <ForteScarlet@163.com>
2021-04-28 01:12:42 +0800

    开黑啦 信令

--- component/component-kaiheila-core/src/main/java/love/forte/simbot/component/kaiheila/event/Event.kt
+++ component/component-kaiheila-core/src/main/java/love/forte/simbot/component/kaiheila/event/Event.kt
@@ -12,14 +12,17 @@
  *
  */
 
-package love.forte.simbot.component.kaiheila
+package love.forte.simbot.component.kaiheila.event
 
-/*
-    占位文件
+
+/**
+ *
+ * 开黑啦Event - [事件Event](https://developer.kaiheila.cn/doc/event)
+ *
+ *
+ * 当 websocket 或 webhook 收到 s=0 的消息时，代表当前收到的消息是事件(包含用户的聊天消息及系统的通知消息等)。
+ *
+ * @author ForteScarlet
  */
-
-public interface Kaiheila
-
-
-// signaling
-// signal
+public interface Event {
+}
