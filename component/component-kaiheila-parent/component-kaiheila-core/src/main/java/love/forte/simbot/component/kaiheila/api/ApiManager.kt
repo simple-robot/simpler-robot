6ec6d6f209cf5d2011aef377e9cb0e6e538467d3
ForteScarlet <ForteScarlet@163.com>
2021-05-25 22:11:49 +0800

    server api for v3

--- component/component-kaiheila-parent/component-kaiheila-core/src/main/java/love/forte/simbot/component/kaiheila/api/ApiManager.kt
+++ component/component-kaiheila-parent/component-kaiheila-core/src/main/java/love/forte/simbot/component/kaiheila/api/ApiManager.kt
@@ -13,36 +13,25 @@
  *  *
  *
  */
+@file:JvmName("KaiheilaApis")
+@file:Suppress("unused")
 
 package love.forte.simbot.component.kaiheila.api
 
 
 /**
+ * 一个[开黑啦的API][https://developer.kaiheila.cn/doc/reference]信息。
  *
- * 开黑啦API管理器，组件在流程中进行阿皮信息的注册。
+ * 其主要记录一个API的版本信息。
  *
- * 目前apiManager的主要作用为管理（可能）存在的多实现api。
- *
- * 截止到目前 (2021/5/25) 为止，开黑啦api还只有一个 `v3` 版本。
+ * [version] 属性将会作为 [KaiheilaApi] 的唯一标识。
  *
  *
- * @author ForteScarlet
- */
-public interface ApiManager {
-
-    fun registerApi(api: Api, /* TODO 对应的sender工厂 */)
-
-
-}
-
-
-/**
- * 一个开黑啦的API信息。其主要记录（注册）一个API的版本信息，并将其注册到 [ApiManager] 中。
- *
- * [version] 属性将会作为 [Api] 的唯一标识。
+ * @see BaseKaiheilaApi
+ * @see KaiheilaApiData
  *
  */
-public interface Api {
+public interface KaiheilaApi {
     /**
      * api的版本编号。以 `v3` 为例，版本号则为 `3`。
      */
@@ -52,17 +41,19 @@ public interface Api {
      * 版本号。以 `v3` 为例，得到的就是 `v3`。
      */
     @JvmDefault
-    val version: String get() = "v$versionNumber"
+    val version: String
+        get() = "v$versionNumber"
 
     /**
      * 得到当前版本的API请求路径。
-     * 默认情况下即为 `${BASE_URL}/${version}`的形式。
+     * 默认情况下即为 `${BASE_URL}/${version}` 的形式。
      */
     @JvmDefault
-    val apiUrl: String get() = "$BASE_URL/$version"
+    val apiUrl: String
+        get() = "$BASE_URL/$version"
 
 
-    companion object {
+    companion object Base {
         /**
          * 开黑啦API的[基础url][https://developer.kaiheila.cn/doc/reference]。
          */
@@ -72,7 +63,33 @@ public interface Api {
 
 
 /**
- * [Api]基础实现。
+ * [KaiheilaApi] 基础抽象类。
  */
-public data class ApiData(override val versionNumber: Int): Api
+public abstract class BaseKaiheilaApi(override val versionNumber: Int) : KaiheilaApi {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
 
+        other as BaseKaiheilaApi
+
+        if (version != other.version) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int = version.hashCode()
+
+    override fun toString(): String = "API(version=$version)"
+}
+
+
+/**
+ * [KaiheilaApi]基础实现。
+ */
+public class KaiheilaApiData(versionNumber: Int) : BaseKaiheilaApi(versionNumber)
+
+
+/**
+ * get instance for [KaiheilaApi] by [KaiheilaApi.versionNumber] value.
+ */
+public inline fun kaiheilaApi(versionNumber: () -> Int) = KaiheilaApiData(versionNumber())
