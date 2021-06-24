b919d3e5c9d96a85619cec065724639623a7ba34
ForteScarlet <ForteScarlet@163.com>
2021-06-24 23:42:33 +0800

    update module name

---
+++ component/component-kaiheila-parent/kaiheila-api-v3/src/main/java/love/forte/simbot/component/kaiheila/api/v3/message/MessageApi.kt
@@ -0,0 +1,36 @@
+/*
+ *
+ *  * Copyright (c) 2021. ForteScarlet All rights reserved.
+ *  * Project  simple-robot
+ *  * File     MiraiAvatar.kt
+ *  *
+ *  * You can contact the author through the following channels:
+ *  * github https://github.com/ForteScarlet
+ *  * gitee  https://gitee.com/ForteScarlet
+ *  * email  ForteScarlet@163.com
+ *  * QQ     1149159218
+ *
+ */
+
+package love.forte.simbot.component.kaiheila.api.v3.message
+
+import love.forte.simbot.component.kaiheila.api.ApiData
+
+
+/**
+ * [频道消息相关接口](https://developer.kaiheila.cn/doc/http/message) 请求实例接口。
+ *
+ *
+ */
+public interface MessageApiReq<RESP : ApiData.Resp<*>> : ApiData.Req<RESP>
+
+
+/**
+ * [频道消息相关接口](https://developer.kaiheila.cn/doc/http/message) 响应实例接口。
+ */
+public interface MessageApiRespData : ApiData.Resp.Data
+
+
+
+
+
