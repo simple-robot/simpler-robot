5a915d935e2066d639e8f6fc4e8ddf05d3361858
ForteScarlet <ForteScarlet@163.com>
2021-06-25 22:59:08 +0800

    gateway test

--- component/component-kaiheila-parent/kaiheila-api-v3/src/test/java/love/test/GatewayApiTest.kt
+++ component/component-kaiheila-parent/kaiheila-api-v3/src/test/java/love/test/GatewayApiTest.kt
@@ -18,11 +18,18 @@ import io.ktor.client.features.websocket.*
 import io.ktor.http.cio.websocket.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.decodeFromJsonElement
+import kotlinx.serialization.json.int
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import love.forte.simbot.component.kaiheila.api.ObjectResp
 import love.forte.simbot.component.kaiheila.api.doRequest
 import love.forte.simbot.component.kaiheila.api.v3.Gateway
 import love.forte.simbot.component.kaiheila.api.v3.GatewayReq
 import love.forte.simbot.component.kaiheila.api.v3.V3
+import love.forte.simbot.component.kaiheila.event.*
+import love.forte.simbot.component.kaiheila.khlJson
 import java.util.zip.InflaterInputStream
 import kotlin.test.Test
 
@@ -67,17 +74,42 @@ class GatewayApiTest {
                     val text = InflaterInputStream(frame.readBytes().inputStream()).reader(Charsets.UTF_8).use {
                         it.readText()
                     }
-
                     println("[Binary-text]: $text")
+
+                    val element = khlJson.parseToJsonElement(text)
+                    val s = element.jsonObject["s"]?.jsonPrimitive?.int
+                    val signal = when(s) {
+                        0 -> khlJson.decodeFromJsonElement<Signal_0>(element)
+                        1 -> khlJson.decodeFromJsonElement<Signal_1>(element)
+                        2 -> khlJson.decodeFromJsonElement<Signal_2>(element)
+                        3 -> khlJson.decodeFromJsonElement<Signal_3>(element)
+                        5 -> khlJson.decodeFromJsonElement<Signal_5>(element)
+                        6 -> khlJson.decodeFromJsonElement<Signal_6>(element)
+                        else -> error(element)
+                    }
+
+                    println("[Signal_$s]: $signal")
+
                 }
-                is Frame.Close -> println("[Close ]: " + frame.readBytes().decodeToString())
-                is Frame.Ping -> println("[Ping  ]: " + frame.readBytes().decodeToString())
-                is Frame.Pong -> println("[Pong: ]: " + frame.readBytes().decodeToString())
+                is Frame.Close -> {
+                    println("[Close ]: " + frame.readBytes().decodeToString())
+                    this.close()
+                }
+                is Frame.Ping -> {
+                    val str = frame.readBytes().decodeToString()
+                    val ping = khlJson.decodeFromString<Signal.Ping>(str)
+
+                    println(ping)
+
+                    println("[Ping  ]: $str")
+                }
+                is Frame.Pong -> {
+                    println("[Pong: ]: " + frame.readBytes().decodeToString())
+                }
             }
 
-            delay(3000)
+            delay(30000)
             this.close()
-
         }
     }
 
