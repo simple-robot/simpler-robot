f4b34c123cf2cf82645ba8c3857c175b215fa2da
ForteScarlet <ForteScarlet@163.com>
2021-09-09 17:58:31 +0800

    intimacy's api

---
+++ component/component-kaiheila-parent/kaiheila-api-v3/src/main/java/love/forte/simbot/component/kaiheila/api/v3/intimacy/IntimacyIndex.kt
@@ -0,0 +1,86 @@
+package love.forte.simbot.component.kaiheila.api.v3.intimacy
+
+import kotlinx.serialization.DeserializationStrategy
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import love.forte.simbot.component.kaiheila.api.*
+
+
+/**
+ *
+ * [获取用户亲密度](https://developer.kaiheila.cn/doc/http/intimacy#获取用户亲密度)
+ *
+ * `/api/v3/intimacy/index`
+ *
+ * method GET
+ *
+ */
+public class IntimacyIndexReq(
+    /**
+     * 用户ID
+     */
+    private val userId: String,
+): GetIntimacyApiReq<ObjectResp<IntimacyIndex>>,
+    BaseApiDataReq<ObjectResp<IntimacyIndex>>(Key) {
+        companion object Key : BaseApiDataKey("intimacy", "index") {
+            private val DATA_SERIALIZER = objectResp<IntimacyIndex>()
+        }
+
+    override val dataSerializer: DeserializationStrategy<ObjectResp<IntimacyIndex>>
+        get() = DATA_SERIALIZER
+
+    override fun createBody(): Any? = null
+
+    override fun RouteInfoBuilder.doRoute() {
+        parameters {
+            append("user_id", userId)
+        }
+    }
+}
+
+
+/**
+ * 好感度信息
+ *
+ *
+ */
+@Serializable
+public data class IntimacyIndex(
+    /**
+     * 机器人给用户显示的形象图片地址
+     */
+    @SerialName("img_url")
+    val imgUrl: String,
+    /**
+     * 机器人显示给用户的社交信息
+     */
+    @SerialName("social_info")
+    val socialInfo: String,
+    @SerialName("last_read")
+    val lastRead: Int,
+    /**
+     * 形象图片的总列表
+     */
+    @SerialName("img_list")
+    val imgList: List<Img>
+) : IntimacyApiRespData {
+
+    /**
+     * 好感度形象图片实例
+     */
+    @Serializable
+    public data class Img(
+        /**
+         * 形象图片的 id
+         */
+        val id: String,
+        /**
+         * 形象图片的地址
+         */
+        val url: String
+        )
+
+}
+
+
+
---
+++ component/component-kaiheila-parent/kaiheila-api-v3/src/main/java/love/forte/simbot/component/kaiheila/api/v3/intimacy/IntimacyUpdate.kt
@@ -0,0 +1,53 @@
+package love.forte.simbot.component.kaiheila.api.v3.intimacy
+
+import io.ktor.http.cio.*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import love.forte.simbot.component.kaiheila.api.BaseApiDataKey
+import love.forte.simbot.component.kaiheila.api.BaseApiDataReq
+import love.forte.simbot.component.kaiheila.api.EmptyResp
+
+
+/**
+ * [更新用户亲密度](https://developer.kaiheila.cn/doc/http/intimacy#更新用户亲密度)
+ *
+ * `/api/v3/intimacy/update`
+ *
+ * method POST
+ */
+public class IntimacyUpdateReq(
+    private val userId: String,
+    private val score: Int? = null,
+    private val socialInfo: String? = null,
+    private val imgId: String? = null
+) :
+    EmptyRespPostIntimacyApiReq,
+    BaseApiDataReq<EmptyResp>(Key) {
+    companion object Key : BaseApiDataKey("intimacy", "update")
+
+    init {
+        require(score in 0..2200) { "Score must in 0 .. 2200" }
+        require(socialInfo?.length?.let { it <= 500 } ?: true) { "Social info must <= 500." }
+    }
+
+    override fun createBody(): Any = Body(userId, score, socialInfo, imgId)
+
+    @Serializable
+    private data class Body(
+        @SerialName("user_id")
+        private val userId: String,
+        private val score: Int?,
+        @SerialName("social_info")
+        private val socialInfo: String?,
+        @SerialName("img_id")
+        private val imgId: String?
+    )
+
+}
+
+/*
+user_id	string	POST	是	用户的 id
+score	int	POST	否	亲密度，0-2200
+social_info	string	POST	否	机器人与用户的社交信息，500 字以内
+img_id	int	POST	否	id必须在用户亲密度接口返回的 img_list 中
+ */
