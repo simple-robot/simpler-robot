7c846f98e3429b32f08b9d2274069a253098df19
ForteScarlet <ForteScarlet@163.com>
2021-09-30 19:17:30 +0800

    :tada: Initial project.

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.31"
+}
+
+group = "love.forte"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "simply-robot"
+
