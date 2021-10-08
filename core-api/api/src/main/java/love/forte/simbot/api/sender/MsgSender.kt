0c9b95fc45babb70d781b1e669bcf4087382c874
ForteScarlet <ForteScarlet@163.com>
2021-10-08 10:21:25 +0800

    For getter suspend function

--- core-api/api/src/main/java/love/forte/simbot/api/sender/MsgSender.kt
+++ core-api/api/src/main/java/love/forte/simbot/api/sender/MsgSender.kt
@@ -16,7 +16,6 @@
 
 package love.forte.simbot.api.sender
 
-import kotlinx.coroutines.runBlocking
 import love.forte.common.utils.Carrier
 import love.forte.simbot.api.message.containers.BotContainer
 import love.forte.simbot.api.message.containers.BotInfo
