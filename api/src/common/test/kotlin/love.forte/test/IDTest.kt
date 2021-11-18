a8deca9917f31c51fbc7d59924af8aa83632662a
ForteScarlet <ForteScarlet@163.com>
2021-11-18 18:40:17 +0800

    ID

--- api/src/common/test/kotlin/love.forte/test/IDTest.kt
+++ api/src/common/test/kotlin/love.forte/test/IDTest.kt
@@ -3,12 +3,12 @@ package love.forte.test
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import love.forte.simbot.ID
+import love.forte.simbot.*
 import kotlin.test.Test
 
 @Serializable
 data class User(
-    val id: LongID,
+    val id: DoubleID,
     val age: Int,
 )
 
@@ -20,7 +20,8 @@ class IDTest {
 
     @Test
     fun idSerializerTest() {
-        val user = User(1.2.ID, 24)
+        // val user = User(100L.ID, 24)
+        val user = User(5.2355.ID, 24)
 
         println(user)
 
