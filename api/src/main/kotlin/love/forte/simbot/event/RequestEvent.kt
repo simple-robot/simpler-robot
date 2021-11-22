577e02c1f5f6394e3afcc5601f3e89679bc37f7d
ForteScarlet <ForteScarlet@163.com>
2021-11-23 00:20:40 +0800

    request event

--- api/src/main/kotlin/love/forte/simbot/event/RequestEvent.kt
+++ api/src/main/kotlin/love/forte/simbot/event/RequestEvent.kt
@@ -8,20 +8,51 @@ import love.forte.simbot.message.doSafeCast
  *
  * 与 **请求** 有关的事件。
  *
+ * 对于一个请求，如果要对其响应，必然
+ *
  * @author ForteScarlet
  */
-public interface RequestEvent : Event {
+public interface RequestEvent<
+        AR, RR,
+        AA : RequestEvent.AcceptAction<AR>,
+        RA : RequestEvent.RejectAction<RR>> : Event {
     override val metadata: Event.Metadata
     override val bot: Bot
 
     /**
-     * 对于请求事件，可见范围普遍为 [Event.VisibleScope.INTERNAL] 或 [Event.VisibleScope.PRIVATE]. 
+     * 对于请求事件，可见范围普遍为 [Event.VisibleScope.INTERNAL] 或 [Event.VisibleScope.PRIVATE].
      */
     override val visibleScope: Event.VisibleScope
 
+    /**
+     *
+     */
+    public suspend fun accept(action: AA): AR
+
+    /**
+     *
+     */
+    public suspend fun <R> reject(action: RA): RR
 
 
-    public companion object Key : BaseEventKey<RequestEvent>("api-request") {
-        override fun safeCast(value: Any): RequestEvent? = doSafeCast(value)
+    public interface AcceptAction<R> {
+
+
+        // TODO
+        public companion object Direct : AcceptAction<Boolean>
+    }
+
+
+    public interface RejectAction<R> {
+
+
+        // TODO
+        public companion object Direct : RejectAction<Boolean>
+    }
+
+
+
+    public companion object Key : BaseEventKey<RequestEvent<*,*,*,*>>("api-request") {
+        override fun safeCast(value: Any): RequestEvent<*,*,*,*>? = doSafeCast(value)
     }
 }
