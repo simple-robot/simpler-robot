5ec9e92b9ef72cf0ca4e68a4b6fd686f35c4c850
ForteScarlet <ForteScarlet@163.com>
2021-12-10 11:41:22 +0800

    logger test

--- spring-boot-starter/component-mirai-spring-boot-starter/src/test/java/test/LoggerTest.kt
+++ spring-boot-starter/component-mirai-spring-boot-starter/src/test/java/test/LoggerTest.kt
@@ -1,27 +1,27 @@
-package test
-
-import kotlinx.coroutines.*
-import kotlinx.coroutines.future.asCompletableFuture
-import net.mamoe.mirai.BotFactory
-import net.mamoe.mirai.utils.runBIO
-import org.slf4j.LoggerFactory
-
-
+// package test
+//
+// import kotlinx.coroutines.*
+// import kotlinx.coroutines.future.asCompletableFuture
+// import net.mamoe.mirai.BotFactory
+// import org.slf4j.LoggerFactory
+//
+//
 // fun main() {
 //     val logger = LoggerFactory.getLogger("1")
 //
+//     val scope = CoroutineScope(Dispatchers.Default)
 //
 //
 //
 //
 //     runCatching {
-//         runBlocking {
-//             runInterruptible {
-//                 runBlocking {
-//                     BotFactory.newBot(1, "1").login()
-//                 }
-//             }
-//         }
+//         val future = scope.async {
+//             BotFactory.newBot(1, "1").login()
+//         }.asCompletableFuture()
+//
+//         future.join()
+//         future.get()
+//
 //     }.getOrElse {
 //         logger.error("err", it)
 //     }
