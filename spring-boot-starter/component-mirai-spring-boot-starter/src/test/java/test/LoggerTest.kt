7d93e1d4e41c73eecfa3790e412aedae246ed232
ForteScarlet <ForteScarlet@163.com>
2021-12-10 13:53:54 +0800

    logger test

--- spring-boot-starter/component-mirai-spring-boot-starter/src/test/java/test/LoggerTest.kt
+++ spring-boot-starter/component-mirai-spring-boot-starter/src/test/java/test/LoggerTest.kt
@@ -1,28 +1,56 @@
-// package test
-//
-// import kotlinx.coroutines.*
-// import kotlinx.coroutines.future.asCompletableFuture
-// import net.mamoe.mirai.BotFactory
-// import org.slf4j.LoggerFactory
-//
-//
-// fun main() {
-//     val logger = LoggerFactory.getLogger("1")
-//
-//     val scope = CoroutineScope(Dispatchers.Default)
-//
-//
-//
-//
-//     runCatching {
-//         val future = scope.async {
-//             BotFactory.newBot(1, "1").login()
-//         }.asCompletableFuture()
-//
-//         future.join()
-//         future.get()
-//
-//     }.getOrElse {
-//         logger.error("err", it)
-//     }
-// }
+package test
+
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import net.mamoe.mirai.utils.withExceptionCollector
+import org.slf4j.LoggerFactory
+
+class MyEx(message: String?, cause: Throwable?) : RuntimeException(message, cause)
+
+fun main() {
+    val logger = LoggerFactory.getLogger("1")
+    val me = MyEx("NO", null)
+    val ce = CancellationException("bot closed.", me)
+    me.addSuppressed(ce)
+
+
+    runCatching {
+        runBlocking {
+            throw CancellationException("bot closed.", me)
+        }
+    }.getOrElse {
+        logger.error("err", it)
+    }
+}
+
+
+suspend fun run(): Int = withExceptionCollector {
+    delay(1)
+    withExceptionCollector {
+        if (System.getProperty("abc") == null) {
+            collectThrow(MyEx("NO", null))
+        } else 1
+    }
+}
+
+/*
+Returning type parameter has been inferred to Nothing implicitly because Nothing is more specific than specified expected type.
+Please specify type arguments explicitly in accordance with expected type to hide this warning.
+Nothing can produce an exception at runtime.
+See KT-36776 for more details.
+ */
+
+
+fun Throwable.showAllSuppressed(index: Int) {
+    println("> $index: $this")
+    this.cause?.let {
+        println("> $index CAUSE: $it")
+        it.showAllSuppressed(index)
+    }
+
+    for (throwable in suppressed) {
+        println("> SUPPRESSED $index -> $throwable")
+        throwable.showAllSuppressed(index + 1)
+    }
+}
