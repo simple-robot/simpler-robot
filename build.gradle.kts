c457ef2208b02bcd7b4805dd5b0bba0a91c844ff
ForteScarlet <ForteScarlet@163.com>
2022-01-16 16:12:35 +0800

    build config

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,8 +79,6 @@ subprojects {
     }
 
 
-
-
 }
 
 
@@ -96,26 +94,23 @@ fun Project.configDokka() {
 }
 
 
-
-
 // nexus staging
 
 
+val credentialsUsername: String? = extra.get("credentials.username")?.toString()
+val credentialsPassword: String? = extra.get("credentials.password")?.toString()
 
+if (credentialsUsername != null && credentialsPassword != null) {
+    nexusPublishing {
+        packageGroup.set(P.Simbot.GROUP)
 
-val credentialsUsername: String = extra.get("credentials.username")!!.toString()
-val credentialsPassword: String = extra.get("credentials.password")!!.toString()
+        repositories {
+            sonatype {
+                username.set(credentialsUsername)
+                password.set(credentialsPassword)
+            }
 
-
-nexusPublishing {
-    packageGroup.set(P.Simbot.GROUP)
-
-    repositories {
-        sonatype {
-            username.set(credentialsUsername)
-            password.set(credentialsPassword)
         }
-
     }
 }
 
@@ -123,7 +118,5 @@ nexusPublishing {
 idea {
     module {
         isDownloadSources = true
-
     }
-
 }
