7a000f62472c979583b0b73b015a58f33462052d
ForteScarlet <ForteScarlet@163.com>
2022-01-25 00:39:08 +0800

    更新、调整、完善开源协议
    Signed-off-by: ForteScarlet <ForteScarlet@163.com>

--- boots/simboot-api/src/main/kotlin/love/forte/simboot/filter/RegexMatcherValue.kt
+++ boots/simboot-api/src/main/kotlin/love/forte/simboot/filter/RegexMatcherValue.kt
@@ -1,11 +1,11 @@
 /*
- *  Copyright (c) 2020-2021 ForteScarlet <https://github.com/ForteScarlet>
+ *  Copyright (c) 2020-2022 ForteScarlet <ForteScarlet@163.com>
  *
- *  根据 Apache License 2.0 获得许可；
+ *  根据 GNU LESSER GENERAL PUBLIC LICENSE 3 获得许可；
  *  除非遵守许可，否则您不得使用此文件。
  *  您可以在以下网址获取许可证副本：
  *
- *       https://www.apache.org/licenses/LICENSE-2.0
+ *       https://www.gnu.org/licenses/lgpl-3.0-standalone.html
  *
  *   有关许可证下的权限和限制的具体语言，请参见许可证。
  */
--- boots/simboot-api/src/main/kotlin/love/forte/simboot/utils/Globs.kt
+++ boots/simboot-api/src/main/kotlin/love/forte/simboot/utils/Globs.kt
@@ -1,26 +1,13 @@
 /*
- * Copyright (c) 2008, 2009, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *  Copyright (c) 2008-2022 ForteScarlet <ForteScarlet@163.com>
  *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ *  根据 GNU LESSER GENERAL PUBLIC LICENSE 3 获得许可；
+ *  除非遵守许可，否则您不得使用此文件。
+ *  您可以在以下网址获取许可证副本：
  *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
+ *       https://www.gnu.org/licenses/lgpl-3.0-standalone.html
  *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
+ *   有关许可证下的权限和限制的具体语言，请参见许可证。
  */
 package love.forte.simboot.utils
 
