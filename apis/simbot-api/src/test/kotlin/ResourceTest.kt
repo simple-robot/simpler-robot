dadfc572f18955028c0b5b0e2e0a0148b7f90609
ForteScarlet <ForteScarlet@163.com>
2022-02-01 22:12:21 +0800

    StandardStreamableResource

--- apis/simbot-api/src/test/kotlin/ResourceTest.kt
+++ apis/simbot-api/src/test/kotlin/ResourceTest.kt
@@ -16,7 +16,7 @@
  */
 
 import kotlinx.serialization.json.Json
-import love.forte.simbot.resources.StreamableResource
+import love.forte.simbot.resources.StandardStreamableResource
 import love.forte.simbot.resources.toResource
 import kotlin.io.path.Path
 
@@ -24,11 +24,11 @@ fun main() {
 
     val r = Path(".").toResource()
 
-    val json = Json.encodeToString(StreamableResource.serializer(), r)
+    val json = Json.encodeToString(StandardStreamableResource.serializer(), r)
 
     println(json)
 
-    val resource = Json.decodeFromString(StreamableResource.serializer(), json)
+    val resource = Json.decodeFromString(StandardStreamableResource.serializer(), json)
 
     println(resource)
 }
