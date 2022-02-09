7aa7c7e8dc3f14e7b9416856cf849c8e0f7d9584
ForteScarlet <ForteScarlet@163.com>
2022-02-09 10:11:40 +0800

    test

--- spring-boot-starter/component-mirai-spring-boot-starter/src/test/java/test/TestApplication.java
+++ spring-boot-starter/component-mirai-spring-boot-starter/src/test/java/test/TestApplication.java
@@ -1,9 +1,15 @@
 package test;
 
 import love.forte.simbot.annotation.SimbotApplication;
+import love.forte.simbot.bot.Bot;
+import love.forte.simbot.bot.BotManager;
+import love.forte.simbot.bot.BotVerifyInfo;
 import love.forte.simbot.spring.autoconfigure.EnableSimbot;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author ForteScarlet
@@ -13,7 +19,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestApplication {
     public static void main(String[] args) {
-            SpringApplication.run(TestApplication.class, args);
-        // SpringApplication : reportFailure : 834
+        SpringApplication.run(TestApplication.class, args);
     }
+
+    @RestController
+    public static class MyController {
+        @Autowired
+        private BotManager manager;
+
+        @GetMapping("/login")
+        public String newBot() {
+            final Bot bot = manager.registerBot(BotVerifyInfo.withCodeVerification("123", "456"));
+            System.out.println("!");
+            return bot.getBotInfo().getBotCode();
+        }
+
+    }
+
 }
