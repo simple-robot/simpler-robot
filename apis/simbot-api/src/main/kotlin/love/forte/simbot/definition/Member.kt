90e20c1f1e43df208ed2529a3b076f510a642586
ForteScarlet <ForteScarlet@163.com>
2022-02-13 16:53:15 +0800

    重新调整Role相关Api

--- apis/simbot-api/src/main/kotlin/love/forte/simbot/definition/Member.kt
+++ apis/simbot-api/src/main/kotlin/love/forte/simbot/definition/Member.kt
@@ -76,14 +76,14 @@ public interface Member : User, MemberInfo, MuteSupport {
      *
      * @see Role.isAdmin
      */
-    public suspend fun isAdmin(): Boolean = roles().firstOrNull { r -> r.isAdmin() } != null
+    public suspend fun isAdmin(): Boolean = roles().firstOrNull { it.isAdmin } != null
 
     /**
      * 判断当前成员是否拥有"拥有者"这样的角色。
      *
      * @see Role.isOwner
      */
-    public suspend fun isOwner(): Boolean = roles().firstOrNull { r -> r.isOwner() } != null
+    public suspend fun isOwner(): Boolean = roles().firstOrNull { it.isOwner } != null
 
     /**
      * 判断当前成员是否拥有"管理者"的权限。
