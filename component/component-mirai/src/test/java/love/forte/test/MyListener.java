44f1eb5c908c18258d21338cbb3cfe6a91ec3598
ForteScarlet <ForteScarlet@163.com>
2022-02-25 15:56:35 +0800

    test

--- component/component-mirai/src/test/java/love/forte/test/MyListener.java
+++ component/component-mirai/src/test/java/love/forte/test/MyListener.java
@@ -2,7 +2,9 @@ package love.forte.test;
 
 import love.forte.common.ioc.annotation.Beans;
 import love.forte.simbot.annotation.OnGroup;
+import love.forte.simbot.annotation.OnPrivate;
 import love.forte.simbot.api.message.events.GroupMsg;
+import love.forte.simbot.api.message.events.PrivateMsg;
 import love.forte.simbot.api.sender.Sender;
 
 /**
@@ -11,9 +13,9 @@ import love.forte.simbot.api.sender.Sender;
 @Beans
 public class MyListener {
 
-    @OnGroup
-    public void quoteTest(GroupMsg msg, Sender sender) {
-        sender.sendGroupMsgAsync(msg, "[CAT:quote] hi!");
+    @OnPrivate
+    public void reply(PrivateMsg msg, Sender sender) {
+        sender.sendPrivateMsgAsync(msg, msg.getMsgContent());
     }
 
 
