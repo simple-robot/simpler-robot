06a6bee9662a922a32e53039403d1638391c0c0e
ForteScarlet <ForteScarlet@163.com>
2022-04-19 22:49:43 +0800

    临时回退版本以发布快照

--- apis/simbot-logger/src/test/kotlin/love/forte/simbot/logger/test/RegexReplaceTest.kt
+++ apis/simbot-logger/src/test/kotlin/love/forte/simbot/logger/test/RegexReplaceTest.kt
@@ -1,7 +1,7 @@
 /*
- *  Copyright (c) 2022-2022 ForteScarlet <https://github.com/ForteScarlet>
+ *  Copyright (c) 2022-2022 ForteScarlet <ForteScarlet@163.com>
  *
- *  本文件是 simply-robot (或称 simple-robot 3.x、simbot 3.x、simbot3) 的一部分。
+ *  本文件是 simply-robot (或称 simple-robot 3.x 、simbot 3.x ) 的一部分。
  *
  *  simply-robot 是自由软件：你可以再分发之和/或依照由自由软件基金会发布的 GNU 通用公共许可证修改之，无论是版本 3 许可证，还是（按你的决定）任何以后版都可以。
  *
@@ -25,6 +25,7 @@ import kotlin.test.Test
 class RegexReplaceTest {
     private val regex = Regex("\\{}")
 
+    //
     @Test
     fun test() {
         val msg = "Hello {}, My {}."
