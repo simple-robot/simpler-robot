d4c8a2df0cad0b94c497a9f710d7e539c5ec00ed
ForteScarlet <ForteScarlet@163.com>
2022-04-22 00:13:27 +0800

    Application Factory实现; 核心 Simple 工厂; fix some

--- boots/simboot-core/src/main/kotlin/love/forte/simboot/core/internal/CoreBootEntranceContextImpl.kt
+++ boots/simboot-core/src/main/kotlin/love/forte/simboot/core/internal/CoreBootEntranceContextImpl.kt
@@ -1,7 +1,7 @@
 /*
- *  Copyright (c) 2021-2022 ForteScarlet <https://github.com/ForteScarlet>
+ *  Copyright (c) 2021-2022 ForteScarlet <ForteScarlet@163.com>
  *
- *  本文件是 simply-robot (或称 simple-robot 3.x、simbot 3.x、simbot3) 的一部分。
+ *  本文件是 simply-robot (或称 simple-robot 3.x 、simbot 3.x ) 的一部分。
  *
  *  simply-robot 是自由软件：你可以再分发之和/或依照由自由软件基金会发布的 GNU 通用公共许可证修改之，无论是版本 3 许可证，还是（按你的决定）任何以后版都可以。
  *
@@ -132,7 +132,8 @@ internal class CoreBootEntranceContextImpl(
             ?.getEventListenerManager()
             ?: coreListenerManager {
                 componentRegistryConfigures.forEach {
-                    it.registerComponent(this)
+                    // TODO auto register
+                    // it.registerComponent(this)
                 }
                 // install all components
                 // installAll()
