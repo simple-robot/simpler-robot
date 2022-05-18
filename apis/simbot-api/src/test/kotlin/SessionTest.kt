1526fbb00f278b3ca9de898b3d700ef5de90f69e
ForteScarlet <ForteScarlet@163.com>
2022-05-18 22:32:59 +0800

    refactor(spring-boot-starter): 简单调整
    简单调整，移除无用参数

--- apis/simbot-api/src/test/kotlin/SessionTest.kt
+++ apis/simbot-api/src/test/kotlin/SessionTest.kt
@@ -1,11 +1,13 @@
+import love.forte.simbot.ExperimentalSimbotApi
 import love.forte.simbot.event.ContinuousSessionContext
 import love.forte.simbot.event.FriendMessageEvent
 import love.forte.simbot.event.invoke
 
+@OptIn(ExperimentalSimbotApi::class)
 suspend fun FriendMessageEvent.testSession(session: ContinuousSessionContext) {
     
     session {
-        val e = waitForNextBlocking()
+        val e = waitingForNext()
     }
     
 }
