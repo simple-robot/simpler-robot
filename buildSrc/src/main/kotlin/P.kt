26b18e23aa7a005ca0c44ba1e91231db61362808
ForteScarlet <ForteScarlet@163.com>
2022-05-23 16:05:22 +0800

    下一个版本
    Signed-off-by: ForteScarlet <ForteScarlet@163.com>

--- buildSrc/src/main/kotlin/P.kt
+++ buildSrc/src/main/kotlin/P.kt
@@ -53,7 +53,7 @@ sealed class P : SimbotProject() {
 
         val version = Version(
             "3", 0, 0,
-            status = preview(10, 1),
+            status = preview(10, 2),
             isSnapshot = System.getProperty("isSnapshot")?.equals("true", true) ?: false
         )
  
