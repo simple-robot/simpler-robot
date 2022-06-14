255b06e55f86c12da06f1f210fd0c024b26cfbe4
ForteScarlet <ForteScarlet@163.com>
2022-06-14 20:43:09 +0800

    核心版本更新到 preview.15.0

--- buildSrc/src/main/kotlin/P.kt
+++ buildSrc/src/main/kotlin/P.kt
@@ -53,7 +53,7 @@ sealed class P : SimbotProject() {
 
         val version = Version(
             "3", 0, 0,
-            status = preview(14, 0),
+            status = preview(15, 0),
             isSnapshot = System.getProperty("isSnapshot")?.equals("true", true) ?: false
         )
  
