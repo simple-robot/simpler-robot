3dd31452793ff0d9c6db130f395a05ff332bf567
ForteScarlet <ForteScarlet@163.com>
2022-06-16 17:07:34 +0800

    依赖版本更新: Kotlinx Coroutines `v1.6.1` -> `v1.6.2`
    Signed-off-by: ForteScarlet <ForteScarlet@163.com>

--- buildSrc/src/main/kotlin/V.kt
+++ buildSrc/src/main/kotlin/V.kt
@@ -97,7 +97,7 @@ sealed class V(group: String?, id: String, version: String?) : Dep(group, id, ve
         // https://github.com/Kotlin/kotlinx.coroutines
         sealed class Coroutines(id: String) : Kotlinx(id = "coroutines-$id", VERSION, true) {
             companion object {
-                const val VERSION = "1.6.1"
+                const val VERSION = "1.6.2"
             }
 
             object J8 : Coroutines("jdk8")
