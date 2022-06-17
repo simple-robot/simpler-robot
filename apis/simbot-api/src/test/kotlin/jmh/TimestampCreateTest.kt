1b3c36c398deac418bc9000b62ae65d210c4a77a
ForteScarlet <ForteScarlet@163.com>
2022-06-17 10:01:20 +0800

    调整测试报告
    Signed-off-by: ForteScarlet <ForteScarlet@163.com>

--- apis/simbot-api/src/test/kotlin/jmh/TimestampCreateTest.kt
+++ apis/simbot-api/src/test/kotlin/jmh/TimestampCreateTest.kt
@@ -6,7 +6,9 @@ import org.openjdk.jmh.annotations.*
 import java.time.Instant
 import java.util.concurrent.TimeUnit
 
-
+/**
+ * 测试 [Timestamp] 实例的两种对当前时间的构建速度。
+ */
 @BenchmarkMode(Mode.Throughput)
 @Measurement(timeUnit = TimeUnit.MILLISECONDS)
 @Threads(8)
