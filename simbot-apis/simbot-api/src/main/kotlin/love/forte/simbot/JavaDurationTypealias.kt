4497ba243e6ed1b40cdcee25e065955154869387
ForteScarlet <ForteScarlet@163.com>
2022-07-05 17:46:29 +0800

    feat(api): JavaDuration与KotlinDuration的转化
    Signed-off-by: ForteScarlet <ForteScarlet@163.com>

--- simbot-apis/simbot-api/src/main/kotlin/love/forte/simbot/JavaDurationTypealias.kt
+++ simbot-apis/simbot-api/src/main/kotlin/love/forte/simbot/JavaDurationTypealias.kt
@@ -18,16 +18,83 @@
 package love.forte.simbot
 
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.nanoseconds
+import kotlin.time.Duration.Companion.seconds
 
 
 public typealias JavaDuration = java.time.Duration
 
 
+/**
+ * 将 [JavaDuration] 转化为 [Duration].
+ *
+ * 在无法优化的情况下，会将 [JavaDuration] 转化为nanos后作为 [Duration] 使用。
+ *
+ */
 public fun JavaDuration.toKotlinDuration(): Duration {
-    TODO()
+    if (this == JavaDuration.ZERO) {
+        return Duration.ZERO
+    }
+    if (nano == 0) {
+        return seconds.seconds
+    }
+    
+    return toNanos().nanoseconds
 }
 
+/**
+ * 将 [Duration] 转化为 [JavaDuration].
+ *
+ * 在无法优化的情况下，会将 [Duration] 转化为nanos后作为 [JavaDuration] 使用。
+ *
+ * 如果 [Duration] 的值为 [Duration.INFINITE], 则会使用 [ifInfinite] 计算结果。
+ * 默认情况下会抛出 [IllegalArgumentException] 异常。
+ *
+ * 如果希望无视 [Duration] 为无穷的情况而直接进行转化，请使用 [Duration.toJavaDurationDirect]。
+ *
+ */
+public inline fun Duration.toJavaDuration(
+    ifInfinite: (duration: Duration) -> JavaDuration = {
+        throw IllegalArgumentException(
+            "Duration is infinite"
+        )
+    },
+): JavaDuration {
+    return toJavaDurationOrNull() ?: ifInfinite(this)
+}
 
-public fun Duration.toJavaDuration(): JavaDuration {
-    TODO()
+/**
+ * 将 [Duration] 转化为 [JavaDuration].
+ *
+ * 在无法优化的情况下，会将 [Duration] 转化为nanos后作为 [JavaDuration] 使用。
+ *
+ * 如果 [Duration] 的值为 [Duration.INFINITE], 则会得到null。
+ *
+ * 如果希望在出现无穷时进行计算，请使用 [Duration.toJavaDuration]；
+ * 如果希望无视 [Duration] 为无穷的情况而直接进行转化，请使用 [Duration.toJavaDurationDirect]。
+ *
+ */
+public fun Duration.toJavaDurationOrNull(): JavaDuration? {
+    if (this.isInfinite()) {
+        return null
+    }
+    
+    return toJavaDurationDirect()
+}
+
+/**
+ * 将 [Duration] 转化为 [JavaDuration].
+ *
+ * 在无法优化的情况下，会将 [Duration] 转化为nanos后作为 [JavaDuration] 使用。
+ * 不会判断 [Duration] 是否为无穷的情况。
+ *
+ * 如果希望在出现无穷时进行计算，请使用 [Duration.toJavaDuration]。
+ *
+ */
+public fun Duration.toJavaDurationDirect(): JavaDuration {
+    if (this == Duration.ZERO) {
+        return JavaDuration.ZERO
+    }
+    
+    return JavaDuration.ofNanos(inWholeNanoseconds)
 }
---
+++ simbot-apis/simbot-api/src/test/kotlin/DurationConvertTest.kt
@@ -0,0 +1,43 @@
+import love.forte.simbot.JavaDuration
+import love.forte.simbot.toJavaDuration
+import love.forte.simbot.toJavaDurationOrNull
+import love.forte.simbot.toKotlinDuration
+import kotlin.test.Test
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.days
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.nanoseconds
+import kotlin.time.Duration.Companion.seconds
+
+/**
+ *
+ * @author ForteScarlet
+ */
+class DurationConvertTest {
+    
+    @Test
+    fun javaToKt() {
+        assert(JavaDuration.ofNanos(1145141919810L).toKotlinDuration() == 1145141919810L.nanoseconds)
+        assert(JavaDuration.ofMillis(114514).toKotlinDuration() == 114514.milliseconds)
+        assert(JavaDuration.ofSeconds(114).toKotlinDuration() == 114.seconds)
+        assert(JavaDuration.ofMinutes(114).toKotlinDuration() == 114.minutes)
+        assert(JavaDuration.ofHours(114).toKotlinDuration() == 114.hours)
+        assert(JavaDuration.ofDays(114).toKotlinDuration() == 114.days)
+    }
+    
+    @Test
+    fun ktToJava() {
+        assert(1145141919810L.nanoseconds.toJavaDuration() == JavaDuration.ofNanos(1145141919810L))
+        assert(114514.milliseconds.toJavaDuration() == JavaDuration.ofMillis(114514))
+        assert(114.seconds.toJavaDuration() == JavaDuration.ofSeconds(114))
+        assert(114.minutes.toJavaDuration() == JavaDuration.ofMinutes(114))
+        assert(114.hours.toJavaDuration() == JavaDuration.ofHours(114))
+        assert(114.days.toJavaDuration() == JavaDuration.ofDays(114))
+        assert(Duration.ZERO.toJavaDuration() == JavaDuration.ZERO)
+        assert(Duration.INFINITE.toJavaDurationOrNull() == null)
+        assert(Duration.INFINITE.unaryMinus().toJavaDurationOrNull() == null)
+    }
+    
+}
