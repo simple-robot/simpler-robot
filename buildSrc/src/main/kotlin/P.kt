243d76cf5089361c3a050f5ea56d228f2b399ea1
ForteScarlet <ForteScarlet@163.com>
2022-07-06 21:54:32 +0800

    release: v3.0.0-beta-M2

--- buildSrc/src/main/kotlin/P.kt
+++ buildSrc/src/main/kotlin/P.kt
@@ -60,7 +60,7 @@ object P {
         
         val version = Version(
             "3", 0, 0,
-            status = VersionStatus.beta(null, null, "-M1"),
+            status = VersionStatus.beta(null, null, "-M2"),
             isSnapshot = isSnapshot()
         )
         
