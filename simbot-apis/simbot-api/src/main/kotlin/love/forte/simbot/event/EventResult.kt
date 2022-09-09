fd8deaf738b4082af2343de6b9ae7bb793f6dea6
ForteScarlet <ForteScarlet@163.com>
2022-09-10 00:28:57 +0800

    feature(api): 将 `EventResult.Default` 访问级别调整为公开

--- simbot-apis/simbot-api/src/main/kotlin/love/forte/simbot/event/EventResult.kt
+++ simbot-apis/simbot-api/src/main/kotlin/love/forte/simbot/event/EventResult.kt
@@ -164,13 +164,13 @@ public interface EventResult {
      *
      * @see Truncated
      */
-    private sealed class Default : EventResult {
-        object Truncated : Default() {
+    public sealed class Default : EventResult {
+        public object Truncated : Default() {
             override val content: Any? get() = null
             override val isTruncated: Boolean get() = true
         }
 
-        object NormalEmpty : Default() {
+        public object NormalEmpty : Default() {
             override val content: Any? get() = null
             override val isTruncated: Boolean get() = false
         }
