d7e5775a820549ab9d59ff821ce2968f80c5f538
ForteScarlet <ForteScarlet@163.com>
2022-09-18 00:28:16 +0800

    build: update kotlin version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,8 +9,8 @@ repositories {
     gradlePluginPortal()
 }
 
-val kotlinVersion = "1.6.21"
-val dokkaPluginVersion = "1.6.21"
+val kotlinVersion = "1.7.10"
+val dokkaPluginVersion = "1.7.10"
 
 dependencies {
     // kotlin("jvm") apply false
@@ -21,6 +21,7 @@ dependencies {
     
     // see https://github.com/gradle-nexus/publish-plugin
     implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
+    implementation("love.forte.plugin.suspend-transform:suspend-transform-plugin-gradle:0.0.1")
 }
 
 val compileKotlin: KotlinCompile by tasks
