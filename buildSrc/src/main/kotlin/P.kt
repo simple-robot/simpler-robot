9bac41b9258f556796af45ee6e769c0f6d44a0f6
ForteScarlet <ForteScarlet@163.com>
2022-09-18 00:17:48 +0800

    build: version调整为 `v3.0.0-beta.2-dev.1`

--- buildSrc/src/main/kotlin/P.kt
+++ buildSrc/src/main/kotlin/P.kt
@@ -60,7 +60,7 @@ object P {
         
         val version = Version(
             "3", 0, 0,
-            status = VersionStatus.beta(2, null, null),
+            status = VersionStatus.beta(2, null, "-dev.1"),
             isSnapshot = isSnapshot()
         )
         
