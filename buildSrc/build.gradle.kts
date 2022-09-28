8ddd1f57678516345f02365599b1682feedff4e8
ForteScarlet <ForteScarlet@163.com>
2022-09-29 02:14:33 +0800

    build: suspend transform version to 0.0.4

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 val kotlinVersion = "1.7.10"
 val dokkaPluginVersion = "1.7.10"
-val suspendTransformVersion = "0.0.3"
+val suspendTransformVersion = "0.0.4"
 
 dependencies {
     // kotlin("jvm") apply false
