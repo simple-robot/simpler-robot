d64bdf256c4e7db1b54e61ce269636f8da9d24cb
ForteScarlet <ForteScarlet@163.com>
2022-10-03 04:53:56 +0800

    feat(core): delayable future api

--- simbot-apis/simbot-api/src/test/java/DelayScopeTest.java
+++ simbot-apis/simbot-api/src/test/java/DelayScopeTest.java
@@ -1,71 +1,33 @@
 import kotlin.coroutines.CoroutineContext;
 import kotlin.coroutines.EmptyCoroutineContext;
-import love.forte.simbot.bot.Bot;
 import love.forte.simbot.ability.DelayableCompletableFuture;
 import love.forte.simbot.ability.DelayableCoroutineScope;
 import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.RepeatedTest;
 
-import java.time.LocalTime;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author ForteScarlet
  */
 public class DelayScopeTest {
 
-    // @Test
-    public void test() throws ExecutionException, InterruptedException {
-        final FooDelayableCoroutineScope scope = new FooDelayableCoroutineScope();
-        final DelayableCompletableFuture<LocalTime> whole = scope.delay(5, TimeUnit.SECONDS, () -> {
-            System.out.println(LocalTime.now());
-        }).delay(5, TimeUnit.SECONDS, () -> {
-            throw new IllegalStateException();
-            // System.out.println(LocalTime.now());
-        }).delay(5, TimeUnit.SECONDS, () -> {
-            System.out.println(LocalTime.now());
-        }).delay(5, TimeUnit.SECONDS, () -> {
-            System.out.println(LocalTime.now());
-        }).delay(5, TimeUnit.SECONDS, () -> {
-            System.out.println(LocalTime.now());
-        }).delay(5, TimeUnit.SECONDS, () -> {
-            System.out.println(LocalTime.now());
-        }).delay(5, TimeUnit.SECONDS, () -> {
-            System.out.println(LocalTime.now());
-        }).delay(5, TimeUnit.SECONDS, () -> {
-            System.out.println(LocalTime.now());
-        }).delay(5, TimeUnit.SECONDS, () -> {
-            System.out.println(LocalTime.now());
-        }).delay(5, TimeUnit.SECONDS, () -> {
-            System.out.println(LocalTime.now());
-        }).delay(5, TimeUnit.SECONDS, () -> {
-            System.out.println(LocalTime.now());
-        }).delay(5, TimeUnit.SECONDS, () -> {
-            System.out.println(LocalTime.now());
-        }).delay(5, TimeUnit.SECONDS, () -> {
-            System.out.println(LocalTime.now());
-        }).delayAndCompute(5, TimeUnit.SECONDS, (v) -> LocalTime.now()); //.map(t -> t.plusSeconds(5));
+    private final FooDelayableCoroutineScope scope = new FooDelayableCoroutineScope();
 
-        Thread.sleep(TimeUnit.SECONDS.toMillis(10));
-        System.out.println("wake up!");
-        System.out.println("whole.get() = " + whole.get());
-    }
 
-    // @Test
-    public void foo(Bot bot) {
-        final DelayableCompletableFuture<LocalTime> whole = bot
-                // (1). 延时5秒，打印当前时间
-                .delay(5, TimeUnit.SECONDS, () -> {
-                    System.out.println(LocalTime.now());
-                })
-                // (2). 流程(1)结束后，再延时5秒，打印当前时间
-                .delay(5, TimeUnit.SECONDS, () -> {
-                    throw new IllegalStateException();
-                    // System.out.println(LocalTime.now());
-                })
-                // (3). 流程(2)结束后，再延时5秒，返回当前时间
-                .delayAndCompute(5, TimeUnit.SECONDS, (v) -> LocalTime.now());
+    @RepeatedTest(10)
+    public void completableApiTest() throws ExecutionException, InterruptedException {
+        final long t1 = ThreadLocalRandom.current().nextLong(500);
+        final long t2 = ThreadLocalRandom.current().nextLong(500);
+        final DelayableCompletableFuture<Long> f1 = scope.delayAndCompute(t1, () -> t1);
+        final DelayableCompletableFuture<Long> f2 = scope.delayAndCompute(t2, () -> t2);
 
+        final DelayableCompletableFuture<Void> either = f1.acceptEitherAsync(f2, (v) -> {
+            assert v == Math.min(t1, t2);
+        });
+
+        either.get();
     }
 
 }
