733477fc828c847123c91240213fb5f81521740c
ForteScarlet <ForteScarlet@163.com>
2022-10-03 23:15:44 +0800

    doc(api): 优先级常量描述

--- simbot-apis/simbot-api/src/main/kotlin/love/forte/simbot/PriorityConstant.kt
+++ simbot-apis/simbot-api/src/main/kotlin/love/forte/simbot/PriorityConstant.kt
@@ -23,7 +23,8 @@ package love.forte.simbot
 public object PriorityConstant {
 
     // 前九个 "较为优先" 级别。
-
+    // 优先级次序为 PRIORITIZED_1 > PRIORITIZED_2 > ... > PRIORITIZED_9
+    
     public const val PRIORITIZED_1: Int = 10
     public const val PRIORITIZED_2: Int = PRIORITIZED_1 + 10
     public const val PRIORITIZED_3: Int = PRIORITIZED_2 + 10
@@ -35,21 +36,26 @@ public object PriorityConstant {
     public const val PRIORITIZED_9: Int = PRIORITIZED_8 + 10
 
     /**
-     * 普通的默认值。存在优先级属性相关的内容，默认使用次值。
+     * 普通地默认值。存在优先级属性相关的内容，默认使用次值。
      */
     public const val NORMAL: Int = 500
 
     /**
+     * 最高的优先级.
+     *
      * 常量中，最高优先级定义为 0，而不是 [Int.MIN_VALUE].
      */
     public const val FIRST: Int = 0
 
     /**
-     * 最后的优先级。其优先级比 [Int.MAX_VALUE] 优先级会高。
+     * 最低的优先级.
+     *
+     * 其优先级会比 [Int.MAX_VALUE] 高。
      */
     public const val LAST: Int = Int.MAX_VALUE - 100
 
     // 后九个 "较为次后" 级别。
+    // 优先级次序为 AFTER_1 > AFTER_2 > ... > AFTER_9
 
     public const val AFTER_1: Int = 1100
     public const val AFTER_2: Int = AFTER_1 + 10
