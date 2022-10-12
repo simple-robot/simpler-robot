618a19a79c1c6d161f6a0af4b207029d6e8f6658
ForteScarlet <ForteScarlet@163.com>
2022-10-13 02:16:10 +0800

    test: log level compare

--- simbot-logger/src/commonMain/kotlin/love/forte/simbot/logger/LogLevel.kt
+++ simbot-logger/src/commonMain/kotlin/love/forte/simbot/logger/LogLevel.kt
@@ -1,5 +1,7 @@
 package love.forte.simbot.logger
 
+import kotlin.jvm.JvmInline
+
 
 /**
  * 日志级别。
@@ -8,3 +10,13 @@ package love.forte.simbot.logger
 public expect enum class LogLevel {
     ERROR, WARN, INFO, DEBUG, TRACE,
 }
+
+@JvmInline
+public value class LogLevelCompare(private val level: LogLevel) {
+    internal inline val compareValue: Int get() = 5 - level.ordinal
+    
+}
+
+public inline val LogLevel.level: LogLevelCompare get() = LogLevelCompare(this)
+
+public operator fun LogLevelCompare.compareTo(other: LogLevelCompare): Int = compareValue.compareTo(other.compareValue)
