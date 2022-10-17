0a4f83e2ce18324ff31686e83b5096f3ee9ac8ef
ForteScarlet <ForteScarlet@163.com>
2022-10-17 17:31:07 +0800

    refactor(utils): simbot-utils and ConcurrentLinkedQueue
    [skip ci]

---
+++ simbot-utils/build.gradle.kts
@@ -0,0 +1,44 @@
+import love.forte.gradle.common.kotlin.multiplatform.defaultConfig
+
+
+plugins {
+    kotlin("multiplatform")
+    `simbot-simple-project-setup`
+    //`simbot-multiplatform-maven-publish`
+    id("simbot.dokka-module-configuration")
+    id("kotlinx-atomicfu")
+}
+
+kotlin {
+    defaultConfig {
+        sourceSetsConfig = {
+            commonMain {
+                dependencies {
+                    api("org.jetbrains.kotlinx:atomicfu:0.18.4")
+                }
+            }
+            commonTest {
+                dependencies {
+                    // implementation(kotlin("test"))
+                    implementation(kotlin("test-annotations-common"))
+                    implementation(kotlin("test-common"))
+                }
+            }
+            jvmMain {
+                dependencies {
+                    api(libs.slf4j.api)
+                }
+            }
+            jvmTest {
+                dependencies {
+                    implementation(kotlin("test-junit5"))
+                }
+            }
+            jsTest {
+                dependencies {
+                    implementation(kotlin("test-js"))
+                }
+            }
+        }
+    }
+}
