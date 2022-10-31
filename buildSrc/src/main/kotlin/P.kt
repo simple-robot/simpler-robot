89de1e859d04e79f2f758d7f8ad7aecc29b97bfe
ForteScarlet <ForteScarlet@163.com>
2022-11-01 02:48:57 +0800

    build: gradle-common upgrade

--- buildSrc/src/main/kotlin/P.kt
+++ buildSrc/src/main/kotlin/P.kt
@@ -74,9 +74,9 @@ sealed class P(override val group: String) : ProjectDetail() {
     val versionWithoutSnapshot: Version
     
     init {
-        val mainVersion = version(4, 0, 0)
+        val mainVersion = version(3, 0, 0)
         //
-        var status = version("M1")
+        var status = version("M2")
         versionWithoutSnapshot = mainVersion - status.copy()
         if (isSnapshot()) {
             status = status - Version.SNAPSHOT
