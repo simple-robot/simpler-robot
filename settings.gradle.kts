58fd7bb557c52a671c64ce534c5f6cc06248909f
ForteScarlet <ForteScarlet@163.com>
2022-12-05 21:34:48 +0800

    CI: workflow test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,11 +32,9 @@ include("simbot-utils:simbot-util-stage-loop")
 
 include("simbot-logger")
 include("simbot-logger-slf4j-impl")
-// project(":simbot-logger").name = "simbot-logger-multiplatform"
 
 include(
     api("api"),
-    // api("logger")
 )
 
 include(core("core"))
@@ -49,15 +47,21 @@ include(
 )
 
 // project test
-include(
-    projectTest("boot"),
-    projectTest("spring-boot-starter"),
-    projectTest("jmh-duration"),
-)
+// if not in CI workflows
+if (!System.getProperty("IS_CI").toBoolean()) {
+    include(
+        projectTest("boot"),
+        projectTest("spring-boot-starter"),
+        projectTest("jmh-duration"),
+    )
 
-include(
-    componentHttpServer("api")
-)
+    include(
+        componentHttpServer("api")
+    )
+}
+
+
+
 
 
 @Suppress("NOTHING_TO_INLINE")
