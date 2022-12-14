7e2405a914bc6d39ffa792284238e64b39210866
ForteScarlet <ForteScarlet@163.com>
2022-12-14 16:20:57 +0800

    ci: snapshot config

--- buildSrc/src/main/kotlin/P.kt
+++ buildSrc/src/main/kotlin/P.kt
@@ -73,7 +73,6 @@ sealed class P(override val group: String) : ProjectDetail() {
     val versionWithoutSnapshot: Version
 
     init {
-        //
         val mainVersion = version(3, 0, 0)
         var status = version("M6")
         versionWithoutSnapshot = mainVersion - status.copy()
