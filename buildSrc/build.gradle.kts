e977ad2465829786342e771c954eb709c88f6e11
ForteScarlet <ForteScarlet@163.com>
2023-01-13 02:10:33 +0800

    build: transform plugin update to 0.2.x

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 val kotlinVersion = "1.8.0"
 val dokkaPluginVersion = "1.7.20"
-val suspendTransformVersion = "0.2.0"
+val suspendTransformVersion = "0.2.2"
 val gradleCommon = "0.0.11"
 
 dependencies {
