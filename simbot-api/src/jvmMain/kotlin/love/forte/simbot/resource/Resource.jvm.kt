310020bee0066b09f66f6d508582fd9dca25565b
ForteScarlet <ForteScarlet@163.com>
2024-02-03 20:34:45 +0800

    pref: JVM 中部分 Resource 实现增加与 Charset 相关的参数或API；优化部分实现的 toString 等内容

--- simbot-api/src/jvmMain/kotlin/love/forte/simbot/resource/Resource.jvm.kt
+++ simbot-api/src/jvmMain/kotlin/love/forte/simbot/resource/Resource.jvm.kt
@@ -36,7 +36,6 @@ import java.nio.file.OpenOption
 import java.nio.file.Path
 import kotlin.io.path.inputStream
 import kotlin.io.path.readBytes
-import kotlin.io.path.readText
 import kotlin.io.path.reader
 
 /**
@@ -51,14 +50,43 @@ public interface InputStreamResource : Resource {
      * 默认通过 [inputStream] 读取。
      *
      */
-    @Throws(Exception::class)
+    @Throws(IOException::class)
     override fun data(): ByteArray = inputStream().use { it.readAllBytes() }
 
     /**
      * 获取可用于读取当前资源数据的输入流。
      */
-    @Throws(Exception::class)
+    @Throws(IOException::class)
     public fun inputStream(): InputStream
+}
+
+/**
+ * 在 JVM 平台下对 [StringResource] 的额外扩展，
+ * 与 [StringResource] 相比，对相关方法增加了 [Charset] 参数。
+ * 默认情况下使用 [Charsets.UTF_8] 格式编码。
+ */
+public interface JVMStringResource : StringResource {
+    /**
+     * 读取此资源的 [String] 内容。
+     * 默认使用 [DEFAULT_CHARSET] 编码。
+     */
+    @Throws(IOException::class)
+    override fun string(): String = string(DEFAULT_CHARSET)
+
+    /**
+     * 读取此资源的 [String] 内容。
+     */
+    @Throws(IOException::class)
+    public fun string(charset: Charset): String
+
+
+    public companion object {
+        /**
+         * 默认编码格式: [Charsets.UTF_8]
+         */
+        @JvmField
+        public val DEFAULT_CHARSET: Charset = Charsets.UTF_8
+    }
 }
 
 /**
@@ -66,18 +94,25 @@ public interface InputStreamResource : Resource {
  *
  * @author forte
  */
-public interface ReaderResource : StringResource {
+public interface ReaderResource : JVMStringResource {
     /**
      * 读取当前资源的字符串数据。
      */
-    @Throws(Exception::class)
-    override fun string(): String = reader().use { it.readText() }
+    @Throws(IOException::class)
+    override fun string(charset: Charset): String = reader(charset).use { it.readText() }
+
+    /**
+     * 获取可用于读取当前资源数据的读取流。
+     * 默认使用 [JVMStringResource.DEFAULT_CHARSET] 编码。
+     */
+    @Throws(IOException::class)
+    public fun reader(): Reader = reader(JVMStringResource.DEFAULT_CHARSET)
 
     /**
      * 获取可用于读取当前资源数据的读取流。
      */
-    @Throws(Exception::class)
-    public fun reader(): Reader
+    @Throws(IOException::class)
+    public fun reader(charset: Charset): Reader
 }
 
 /**
@@ -100,11 +135,19 @@ public interface FileResource : InputStreamResource, ReaderResource {
     override fun inputStream(): InputStream = file.inputStream()
 
     /**
+     * 从与此资源关联的 [File] 创建新的 [Reader]。
+     * 默认使用 [JVMStringResource.DEFAULT_CHARSET] 编码。
+     * @throws FileNotFoundException 如果文件不存在
+     */
+    @Throws(FileNotFoundException::class)
+    override fun reader(): Reader = reader(JVMStringResource.DEFAULT_CHARSET)
+
+    /**
      * 从与此资源关联的 [File] 创建新的 [Reader]
      * @throws FileNotFoundException 如果文件不存在
      */
     @Throws(FileNotFoundException::class)
-    override fun reader(): Reader
+    override fun reader(charset: Charset): Reader
 
     /**
      * 将与此资源关联的 [File] 读取为字节数组
@@ -114,25 +157,56 @@ public interface FileResource : InputStreamResource, ReaderResource {
     override fun data(): ByteArray = file.readBytes()
 
     /**
+     * 将与此资源关联的 [File] 读取为字符串。
+     * 默认使用 [JVMStringResource.DEFAULT_CHARSET] 编码。
+     * @throws IOException 如果文件无法读取
+     */
+    @Throws(IOException::class)
+    override fun string(): String = string(JVMStringResource.DEFAULT_CHARSET)
+
+    /**
      * 将与此资源关联的 [File] 读取为字符串
      * @throws IOException 如果文件无法读取
      */
     @Throws(IOException::class)
-    override fun string(): String
+    override fun string(charset: Charset): String
 }
 
 /**
  * Converts a [File] to a [FileResource].
+ * [charset] 会作为需要使用编码参数的方法的默认编码。
+ * 默认使用 [JVMStringResource.DEFAULT_CHARSET]。
  *
  * @return The converted [FileResource].
  */
 @JvmName("valueOf")
 @JvmOverloads
-public fun File.toResource(charset: Charset = Charsets.UTF_8): FileResource = FileResourceImpl(this, charset)
+public fun File.toResource(charset: Charset = JVMStringResource.DEFAULT_CHARSET): FileResource =
+    FileResourceImpl(this, charset)
 
 private data class FileResourceImpl(override val file: File, private val charset: Charset) : FileResource {
-    override fun string(): String = file.readText(charset)
-    override fun reader(): Reader = file.reader(charset)
+    override fun string(): String = string(charset)
+    override fun reader(): Reader = reader(charset)
+
+    override fun string(charset: Charset): String = file.readText(charset)
+    override fun reader(charset: Charset): Reader = file.reader(charset)
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is FileResourceImpl) return false
+
+        if (file != other.file) return false
+        if (charset != other.charset) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = file.hashCode()
+        result = 31 * result + charset.hashCode()
+        return result
+    }
+
+    override fun toString(): String = "FileResource(file=$file, charset=$charset)"
 }
 
 /**
@@ -155,49 +229,56 @@ public interface PathResource : InputStreamResource, ReaderResource {
     override fun inputStream(): InputStream
 
     /**
-     * 从与此资源关联的 [Path] 创建新的 [Reader]
-     * @throws IOException 如果路径无法打开
-     */
-    @Throws(IOException::class)
-    override fun reader(): Reader
-
-    /**
      * 将与此资源关联的 [Path] 读取为字节数组
      * @throws Exception 如果在路径上执行该操作时出现错误
      */
-    @Throws(Exception::class)
+    @Throws(IOException::class)
     override fun data(): ByteArray = path.readBytes()
+
+    /**
+     * 从与此资源关联的 [Path] 创建新的 [Reader]。
+     * @throws IOException 如果路径无法打开
+     */
+    @Throws(IOException::class)
+    override fun reader(charset: Charset): Reader
 
     /**
      * 将与此资源关联的 [Path] 读取为字符串
      * @throws Exception 如果在路径上执行该操作时出现错误
      */
-    @Throws(Exception::class)
-    override fun string(): String
+    @Throws(IOException::class)
+    override fun string(charset: Charset): String
 }
 
 /**
  * Converts the given [Path] to a [PathResource] with the specified options.
  *
+ *
+ * @param charset 读取内容如果需要 [Charset] 信息时的默认值
  * @param options the options to use for opening the resource (vararg)
  * @return the [PathResource] representing the converted path
  */
 @JvmName("valueOf")
 @JvmOverloads
-public fun Path.toResource(charset: Charset = Charsets.UTF_8, vararg options: OpenOption): PathResource =
+public fun Path.toResource(
+    charset: Charset = JVMStringResource.DEFAULT_CHARSET,
+    vararg options: OpenOption
+): PathResource =
     PathResourceImpl(this, charset, options)
 
 private data class PathResourceImpl(
     override val path: Path,
     private val charset: Charset,
     private val openOptions: Array<out OpenOption>
-) :
-    PathResource {
+) : PathResource {
     override fun inputStream(): InputStream = path.inputStream(options = openOptions)
 
-    override fun reader(): Reader = path.reader(charset, options = openOptions)
+    override fun reader(): Reader = reader(charset)
+    override fun string(): String = string(charset)
 
-    override fun string(): String = path.readText(charset)
+    override fun reader(charset: Charset): Reader = path.reader(charset, options = openOptions)
+    override fun string(charset: Charset): String = reader(charset).use(Reader::readText)
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is PathResourceImpl) return false
@@ -215,6 +296,16 @@ private data class PathResourceImpl(
         result = 31 * result + openOptions.contentHashCode()
         return result
     }
+
+    override fun toString(): String = buildString {
+        append("PathResource(path=")
+        append(path)
+        append(", charset=")
+        append(charset)
+        append(", openOptions=")
+        openOptions.joinTo(buffer = this, separator = ", ", prefix = "[", postfix = "]", limit = 8)
+        append(")")
+    }
 }
 
 /**
@@ -222,7 +313,7 @@ private data class PathResourceImpl(
  *
  * @author forte
  */
-public interface URIResource : InputStreamResource, StringResource {
+public interface URIResource : InputStreamResource, JVMStringResource {
     /**
      * 与此资源关联的 [URI]
      */
@@ -248,7 +339,17 @@ public interface URIResource : InputStreamResource, StringResource {
      * @throws IOException 如果无法打开输入流，则抛出此异常。具体参看 [URL.openStream][java.net.URL.openStream]
      */
     @Throws(IOException::class)
-    override fun string(): String
+    override fun string(): String = super.string()
+
+    /**
+     * 读取 [uri] 中的内容并作为字符串返回。
+     *
+     * @throws IllegalArgumentException see [URI.toURL]
+     * @throws MalformedURLException see [URI.toURL]
+     * @throws IOException 如果无法打开输入流，则抛出此异常。具体参看 [URL.openStream][java.net.URL.openStream]
+     */
+    @Throws(IOException::class)
+    override fun string(charset: Charset): String
 }
 
 /**
@@ -261,22 +362,26 @@ public interface URIResource : InputStreamResource, StringResource {
  * 取而代之的是 [URL.toResource] 可能会产生 [URISyntaxException]，
  * 因为需要使用 [URL.toURI]。
  *
+ * @param charset 需要使用编码格式时的默认编码值，默认为 [JVMStringResource.DEFAULT_CHARSET]。
  * @throws URISyntaxException see [URL.toURI]
  * @return The converted [URIResource].
  */
 @kotlin.jvm.Throws(URISyntaxException::class)
 @JvmName("valueOf")
 @JvmOverloads
-public fun URL.toResource(charset: Charset = Charsets.UTF_8): URIResource = URIResourceImpl(toURI(), charset, this)
+public fun URL.toResource(charset: Charset = JVMStringResource.DEFAULT_CHARSET): URIResource =
+    URIResourceImpl(toURI(), charset, this)
 
 /**
  * Converts the current [URI] to a [URIResource].
  *
+ * @param charset 需要使用编码格式时的默认编码值，默认为 [JVMStringResource.DEFAULT_CHARSET]。
  * @return The converted [URIResource].
  */
 @JvmName("valueOf")
 @JvmOverloads
-public fun URI.toResource(charset: Charset = Charsets.UTF_8): URIResource = URIResourceImpl(this, charset, null)
+public fun URI.toResource(charset: Charset = JVMStringResource.DEFAULT_CHARSET): URIResource =
+    URIResourceImpl(this, charset, null)
 
 private class URIResourceImpl(override val uri: URI, val charset: Charset, private var url: URL? = null) : URIResource {
     private val urlValue: URL
@@ -285,7 +390,8 @@ private class URIResourceImpl(override val uri: URI, val charset: Charset, priva
         }
 
     override fun inputStream(): InputStream = urlValue.openStream()
-    override fun string(): String = urlValue.readText(charset)
+    override fun string(): String = string(charset)
+    override fun string(charset: Charset): String = urlValue.readText(charset)
 
-    override fun toString(): String = "URIResourceImpl(uri=$uri, charset=$charset)"
+    override fun toString(): String = "URIResource(uri=$uri, charset=$charset)"
 }
