0dcbd707f675ea70f50681b82578a17276cf0efb
ForteScarlet <ForteScarlet@163.com>
2024-02-15 22:05:39 +0800

    fix: suspend test timeout

--- simbot-commons/simbot-common-suspend-runner/src/jvmTest/java/JavaBlockingRunTest.java
+++ simbot-commons/simbot-common-suspend-runner/src/jvmTest/java/JavaBlockingRunTest.java
@@ -23,98 +23,73 @@
 
 import love.forte.simbot.suspendrunner.SuspendFoo;
 import love.forte.simbot.suspendrunner.reserve.SuspendReserves;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author ForteScarlet
  */
 public class JavaBlockingRunTest {
 
-    private static Scheduler parallelScheduler;
-
-    @BeforeAll
-    public static void initScheduler() {
-        parallelScheduler = Schedulers.newParallel("parallel", 4);
-    }
-
-    @AfterAll
-    public static void disposeScheduler() {
-        parallelScheduler.dispose();
-    }
+//    private static Scheduler parallelScheduler;
+//
+//    @BeforeAll
+//    public static void initScheduler() {
+//        parallelScheduler = Schedulers.newParallel("parallel", 4);
+//    }
+//
+//    @AfterAll
+//    public static void disposeScheduler() {
+//        parallelScheduler.dispose();
+//    }
 
     private static final String EXPECT_NAME = "forte";
 
-    private void checkDuration(long startNano) {
-        final var now = System.nanoTime();
-        Assertions.assertTrue((now - startNano) > TimeUnit.MILLISECONDS.toNanos(100));
-    }
-
-    private void checkDuration(long startNano, long expectDurationNano) {
-        final var now = System.nanoTime();
-        Assertions.assertTrue((now - startNano) > expectDurationNano);
-    }
 
     @Test
     public void blockingRun() {
-        final var start = System.nanoTime();
         final var foo = new SuspendFoo();
         final var name = foo.runReserve(EXPECT_NAME).transform(SuspendReserves.block());
         Assertions.assertEquals(name, EXPECT_NAME);
-        checkDuration(start);
     }
 
     @Test
     public void asyncRun() {
-        final var start = System.nanoTime();
         final var foo = new SuspendFoo();
         final var nameFuture = foo.runReserve(EXPECT_NAME).transform(SuspendReserves.async());
         final var name = nameFuture.join();
 
         Assertions.assertEquals(name, EXPECT_NAME);
-        checkDuration(start);
     }
 
     @Test
     public void monoRun() {
-        final var start = System.nanoTime();
         final var foo = new SuspendFoo();
         final var nameMono = foo.runReserve(EXPECT_NAME).transform(SuspendReserves.mono());
         final var duration = Duration.ofSeconds(5);
         final var name = nameMono.block(duration);
-        // .subscribeOn(parallelScheduler)
 
         Assertions.assertEquals(name, EXPECT_NAME);
-        checkDuration(start, duration.toNanos());
     }
 
     @Test
     public void rx2Run() {
-        final var start = System.nanoTime();
         final var foo = new SuspendFoo();
         final var nameMaybe = foo.runReserve(EXPECT_NAME).transform(SuspendReserves.rx2Maybe());
         final var name = nameMaybe.blockingGet();
 
         Assertions.assertEquals(name, EXPECT_NAME);
-        checkDuration(start);
     }
 
     @Test
     public void rx3Run() {
-        final var start = System.nanoTime();
         final var foo = new SuspendFoo();
         final var nameMaybe = foo.runReserve(EXPECT_NAME).transform(SuspendReserves.rx3Maybe());
         final var name = nameMaybe.blockingGet();
 
         Assertions.assertEquals(name, EXPECT_NAME);
-        checkDuration(start);
     }
 
 }
