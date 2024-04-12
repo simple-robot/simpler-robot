17f1533cc3beadd64271fde1131439480662ba6d
ForteScarlet <ForteScarlet@163.com>
2024-04-13 00:41:26 +0800

    StringResource 拆分为 StringReadableResource；增加 Resource 和 OfflineImage 的 Resolver 来允许组件等第三方更快速的分流它们的可能内容物

--- simbot-api/src/jvmTest/kotlin/ResourceTests.jvm.kt
+++ simbot-api/src/jvmTest/kotlin/ResourceTests.jvm.kt
@@ -4,7 +4,7 @@
  *     Project    https://github.com/simple-robot/simpler-robot
  *     Email      ForteScarlet@163.com
  *
- *     This file is part of the Simple Robot Library.
+ *     This file is part of the Simple Robot Library (Alias: simple-robot, simbot, etc.).
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Lesser General Public License as published by
@@ -53,7 +53,7 @@ import kotlin.test.assertEquals
  *
  */
 
-class ResourceTests {
+class ResourceResolverTests {
     @Test
     fun fileResourceTest() {
         val file = File.createTempFile("simbot-file-resource", "test", File(".")).also {
