d741b8617c25b666d326fe0486b38db34b6cd298
ForteScarlet <ForteScarlet@163.com>
2024-08-01 03:04:04 +0800

    fix(quantcat): 优化统一MergedBinder对null结果、失败结果的处理

--- simbot-quantcat/simbot-quantcat-common/src/commonMain/kotlin/love/forte/simbot/quantcat/common/annotations/FilterValue.kt
+++ simbot-quantcat/simbot-quantcat-common/src/commonMain/kotlin/love/forte/simbot/quantcat/common/annotations/FilterValue.kt
@@ -4,7 +4,7 @@
  *     Project    https://github.com/simple-robot/simpler-robot
  *     Email      ForteScarlet@163.com
  *
- *     This file is part of the Simple Robot Library.
+ *     This file is part of the Simple Robot Library (Alias: simple-robot, simbot, etc.).
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Lesser General Public License as published by
@@ -26,14 +26,17 @@ package love.forte.simbot.quantcat.common.annotations
 import love.forte.simbot.quantcat.common.filter.FilterValueProperties
 
 /**
- * 指定一个参数，此参数为通过 [love.forte.simbot.quantcat.annotations.Filter]
+ * 指定一个参数，此参数为通过 [love.forte.simbot.quantcat.common.annotations.Filter]
  * 解析而得到的动态参数提取器中的内容。
  *
  * 参数提取格式基于正则匹配模式，参考 [Filter.value] 中的相关说明。
  *
  * @param value 所需动态参数的key。
  * @param required 对于参数绑定器来讲其是否为必须的。
- *  如果不是必须的，则在无法获取参数后传递null作为结果，否则将会抛出异常并交由后续绑定器处理。
+ * 如果不是必须的，则在无法获取参数后传递 `null` 作为结果
+ * (此结果被视为正确结果。换言之如果参数为 `nullable`，
+ * 但是存在一个不是 `null` 的默认值，则最终的参数值依然为 `null`)，
+ * 否则将会抛出异常并交由后续绑定器处理。
  */
 @Target(AnnotationTarget.VALUE_PARAMETER)
 public annotation class FilterValue(val value: String, val required: Boolean = true)
