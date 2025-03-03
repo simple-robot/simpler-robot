e25f03b5354325c5f8896ad2bde999ddb1335c4b
ForteScarlet <ForteScarlet@163.com>
2025-03-03 19:58:12 +0800

    improve(docs): 优化Dokka配置

--- simbot-cores/simbot-core/src/commonTest/kotlin/love/forte/simbot/core/application/ApplicationEventDispatcherTests.kt
+++ simbot-cores/simbot-core/src/commonTest/kotlin/love/forte/simbot/core/application/ApplicationEventDispatcherTests.kt
@@ -1,10 +1,10 @@
 /*
- *     Copyright (c) 2024. ForteScarlet.
+ *     Copyright (c) 2024-2025. ForteScarlet.
  *
  *     Project    https://github.com/simple-robot/simpler-robot
  *     Email      ForteScarlet@163.com
  *
- *     This file is part of the Simple Robot Library.
+ *     This file is part of the Simple Robot Library (Alias: simple-robot, simbot, etc.).
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Lesser General Public License as published by
@@ -108,6 +108,7 @@ class ApplicationEventDispatcherTests {
 
     }
 
+    @OptIn(FuzzyEventTypeImplementation::class)
     private class MyEvent : Event {
         override val id: ID = UUID.random()
 
