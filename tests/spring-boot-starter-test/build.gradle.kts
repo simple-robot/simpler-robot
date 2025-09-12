19a1b05ad7eac3c29e5282d3df8ea19377bd7200
ForteScarlet <ForteScarlet@163.com>
2025-09-12 13:21:27 +0800

    feat: Add experimental Wasm JS interop annotations and new API definitions
    Signed-off-by: ForteScarlet <ForteScarlet@163.com>

--- tests/spring-boot-starter-test/build.gradle.kts
+++ tests/spring-boot-starter-test/build.gradle.kts
@@ -1,4 +1,29 @@
 /*
+ *     Copyright (c) 2025. ForteScarlet.
+ *
+ *     Project    https://github.com/simple-robot/simpler-robot
+ *     Email      ForteScarlet@163.com
+ *
+ *     This file is part of the Simple Robot Library (Alias: simple-robot, simbot, etc.).
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     Lesser GNU General Public License for more details.
+ *
+ *     You should have received a copy of the Lesser GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+import org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation
+
+/*
  *     Copyright (c) 2024. ForteScarlet.
  *
  *     Project    https://github.com/simple-robot/simpler-robot
@@ -48,6 +73,11 @@ dependencies {
 }
 
 kotlin {
+    @OptIn(ExperimentalAbiValidation::class)
+    abiValidation {
+        enabled = false
+    }
+
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
     }
