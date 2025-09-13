30ca43605673eb0e6ff072672144399f7ba81acc
ForteScarlet <ForteScarlet@163.com>
2025-09-13 16:43:12 +0800

    release: v4.14.0
    Signed-off-by: ForteScarlet <ForteScarlet@163.com>

--- buildSrc/src/main/kotlin/P.kt
+++ buildSrc/src/main/kotlin/P.kt
@@ -47,8 +47,8 @@ fun isSnapshot(): Boolean = _isSnapshot
 @Suppress("MemberVisibilityCanBePrivate")
 sealed class P(override val group: String) : ProjectDetail() {
     companion object {
-        const val VERSION = "4.13.0"
-        const val NEXT_VERSION = "4.14.0"
+        const val VERSION = "4.14.0"
+        const val NEXT_VERSION = "4.14.1"
         const val SNAPSHOT_VERSION = "$VERSION-SNAPSHOT"
         const val NEXT_SNAPSHOT_VERSION = "$NEXT_VERSION-SNAPSHOT"
 
