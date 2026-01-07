adaf5ddb51ff84259a651c9ad0e39859feb91955
ForteScarlet <ForteScarlet@163.com>
2026-01-07 17:28:34 +0800

    build: 配置子项目的ABI验证逻辑
    Signed-off-by: ForteScarlet <ForteScarlet@163.com>

--- internal-processors/build.gradle.kts
+++ internal-processors/build.gradle.kts
@@ -1,4 +1,32 @@
 /*
+ *     Copyright (c) 2026. ForteScarlet.
+ *
+ *     Project    https://github.com/simple-robot/simpler-robot
+ *     Email      ForteScarlet@163.com
+ *
+ *     This file is part of the Simple Robot Library (Alias: simple-robot, simbot, etc.).
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     Lesser GNU General Public License for more details.
+ *
+ *     You should have received a copy of the Lesser GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.abi.AbiValidationExtension
+import org.jetbrains.kotlin.gradle.dsl.abi.AbiValidationMultiplatformExtension
+import org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation
+
+/*
  *     Copyright (c) 2024. ForteScarlet.
  *
  *     Project    https://github.com/simple-robot/simpler-robot
@@ -21,7 +49,32 @@
  *
  */
 
+plugins {
+    kotlin("multiplatform") apply false
+}
 
 subprojects {
     group = P.GROUP + ".internal"
+
+    @OptIn(ExperimentalAbiValidation::class)
+    afterEvaluate {
+        // Configures ABI validation based on Kotlin plugin type
+        when {
+            plugins.hasPlugin("org.jetbrains.kotlin.jvm") -> {
+                extensions.configure<KotlinJvmProjectExtension>("kotlin") {
+                    extensions.configure<AbiValidationExtension>("abiValidation") {
+                        enabled.convention(false)
+                    }
+                }
+            }
+
+            plugins.hasPlugin("org.jetbrains.kotlin.multiplatform") -> {
+                extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension> {
+                    extensions.configure<AbiValidationMultiplatformExtension>("abiValidation") {
+                        enabled.convention(false)
+                    }
+                }
+            }
+        }
+    }
 }
