5314f8b2574e173cec745db09452494d43d15514
ForteScarlet <ForteScarlet@163.com>
2026-01-20 13:38:20 +0800

    fix(atomic): 修正部分测试内容代码
    Signed-off-by: ForteScarlet <ForteScarlet@163.com>

--- simbot-commons/simbot-common-collection/src/nativeTest/kotlin/AtomicTest.kt
+++ simbot-commons/simbot-common-collection/src/nativeTest/kotlin/AtomicTest.kt
@@ -1,3 +1,26 @@
+/*
+ *     Copyright (c) 2026. ForteScarlet.
+ *
+ *     Project    https://github.com/simple-robot/simpler-robot
+ *     Email      ForteScarlet@163.com
+ *
+ *     This file is part of the Simple Robot Library (Alias: simple-robot, simbot, etc.).
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     Lesser GNU General Public License for more details.
+ *
+ *     You should have received a copy of the Lesser GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.test.runTest
@@ -20,7 +43,7 @@ class AtomicTest {
     private fun <K, V> Map<K, V>.box(): MapBox<K, V> = MapBox(this)
 
     @Test
-    fun a() = runTest {
+    fun nativeAtomicTest() = runTest {
         val pJob = Job()
         val key = Any()
         val mapRef = AtomicReference(emptyMap<Any, Session<Int, String>>().box())
@@ -31,7 +54,7 @@ class AtomicTest {
             val session = Session(channel)
 
             sJob.invokeOnCompletion {
-                channel.cancel(it?.let { e -> CancellationException(e.message, e) })
+                channel.close(it)
                 // remove session
                 do {
                     val oldMapBox = mapRef.value
