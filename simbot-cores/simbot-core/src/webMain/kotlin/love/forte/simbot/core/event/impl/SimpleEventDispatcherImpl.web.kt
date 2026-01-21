2bbcccd58a1dbb9e973d45ede39b1a7e88922bca
ForteScarlet <ForteScarlet@163.com>
2026-01-21 13:56:06 +0800

    feat(core): 调整改进 core 模块源码集结构，合并 js 和 wasmJs 到 web
    Signed-off-by: ForteScarlet <ForteScarlet@163.com>

--- simbot-cores/simbot-core/src/webMain/kotlin/love/forte/simbot/core/event/impl/SimpleEventDispatcherImpl.web.kt
+++ simbot-cores/simbot-core/src/webMain/kotlin/love/forte/simbot/core/event/impl/SimpleEventDispatcherImpl.web.kt
@@ -1,10 +1,10 @@
 /*
- *     Copyright (c) 2024. ForteScarlet.
+ *     Copyright (c) 2024-2026. ForteScarlet.
  *
  *     Project    https://github.com/simple-robot/simpler-robot
  *     Email      ForteScarlet@163.com
  *
- *     This file is part of the Simple Robot Library.
+ *     This file is part of the Simple Robot Library (Alias: simple-robot, simbot, etc.).
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Lesser General Public License as published by
