8ae3be7d9083a240305df4a1fe46959390af3d91
ForteScarlet <ForteScarlet@163.com>
2026-09-22 09:42:55 +0800

    refactor: 调整 QQ 组件 API 权限相关数据模型为普通类，新增兼容性支持，优化方法及结构定义

--- simbot-component-qq/simbot-component-qq-api/src/commonMain/kotlin/love/forte/simbot/qguild/model/ApiPermission.kt
+++ simbot-component-qq/simbot-component-qq-api/src/commonMain/kotlin/love/forte/simbot/qguild/model/ApiPermission.kt
@@ -28,33 +28,27 @@ import kotlinx.serialization.Serializable
 import love.forte.simbot.qguild.api.ApiDescription
 import love.forte.simbot.qguild.common.ApiModel
 import love.forte.simbot.qguild.common.ApiModelConstructor
+import love.forte.simbot.qguild.common.DataClassCompatibilities
 import kotlin.jvm.JvmName
 import kotlin.jvm.JvmStatic
 
 /**
  * [接口权限对象](https://bot.q.qq.com/wiki/develop/api/openapi/api_permissions/model.html)
  *
+ * @property path API 接口名，例如 `/guilds/{guild_id}/members/{user_id}`
+ * @property method 请求方法，例如 `GET`
+ * @property desc API 接口名称，例如 `获取频道信`
+ * @property authStatus 授权状态，`auth_stats` 为 1 时已授权
  */
 @ApiModel
 @Serializable
-public data class ApiPermission @ApiModelConstructor constructor(
-    /**
-     * API 接口名，例如 `/guilds/{guild_id}/members/{user_id}`
-     */
-    val path: String,
-    /**
-     * 请求方法，例如 `GET`
-     */
-    val method: String,
-    /**
-     * API 接口名称，例如 `获取频道信`
-     */
-    val desc: String,
-    /**
-     * 授权状态，`auth_stats` 为 1 时已授权
-     */
+public class ApiPermission @ApiModelConstructor constructor(
+    public val path: String,
+    public val method: String,
+    public val desc: String,
+
     @SerialName("auth_status")
-    val authStatus: Int
+    public val authStatus: Int
 ) {
     public companion object {
         /**
@@ -62,60 +56,160 @@ public data class ApiPermission @ApiModelConstructor constructor(
          */
         public const val AUTH_STATUS_AUTHORIZED: Int = 1
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ApiPermission) return false
+
+        if (path != other.path) return false
+        if (method != other.method) return false
+        if (desc != other.desc) return false
+        if (authStatus != other.authStatus) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = path.hashCode()
+        result = 31 * result + method.hashCode()
+        result = 31 * result + desc.hashCode()
+        result = 31 * result + authStatus
+        return result
+    }
+
+    override fun toString(): String {
+        return "ApiPermission(path='$path', method='$method', desc='$desc', authStatus=$authStatus)"
+    }
+
+    //region data-class 兼容
+    @Deprecated(DataClassCompatibilities.DEPRECATED_MESSAGE, ReplaceWith("path"))
+    public operator fun component1(): String = path
+
+    @Deprecated(DataClassCompatibilities.DEPRECATED_MESSAGE, ReplaceWith("method"))
+    public operator fun component2(): String = method
+
+    @Deprecated(DataClassCompatibilities.DEPRECATED_MESSAGE, ReplaceWith("desc"))
+    public operator fun component3(): String = desc
+
+    @Deprecated(DataClassCompatibilities.DEPRECATED_MESSAGE, ReplaceWith("authStatus"))
+    public operator fun component4(): Int = authStatus
+
+    @Suppress("DeprecatedCallableAddReplaceWith")
+    @Deprecated(DataClassCompatibilities.DEPRECATED_MESSAGE)
+    public fun copy(
+        path: String = this.path,
+        method: String = this.method,
+        desc: String = this.desc,
+        authStatus: Int = this.authStatus,
+    ): ApiPermission = ApiPermission(path, method, desc, authStatus)
+    //endregion
 }
 
 /**
  * 判断 [ApiPermission.authStatus] 的值是否为 [`1`][ApiPermission.AUTH_STATUS_AUTHORIZED]
  */
 public inline val ApiPermission.isAuthorized: Boolean
-    get() = authStatus == 1
+    get() = authStatus == ApiPermission.AUTH_STATUS_AUTHORIZED
 
 
 /**
- * [接口权限需求对象（APIPermissionDemand）](https://bot.q.qq.com/wiki/develop/api/openapi/api_permissions/model.html#%E6%8E%A5%E5%8F%A3%E6%9D%83%E9%99%90%E9%9C%80%E6%B1%82%E5%AF%B9%E8%B1%A1-apipermissiondemand)
+ * [接口权限需求对象（APIPermissionDemand）](https://bot.q.qq.com/wiki/develop/api-v2/server-inter/channel/api_permissions/model.html#%E6%8E%A5%E5%8F%A3%E6%9D%83%E9%99%90%E9%9C%80%E6%B1%82%E5%AF%B9%E8%B1%A1-apipermissiondemand)
+ *
+ * @property guildId 申请接口权限的频道 id
+ * @property channelId 接口权限需求授权链接发送的子频道 id
+ * @property apiIdentify 权限接口唯一标识
+ * @property title 接口权限链接中的接口权限描述信息
+ * @property desc 接口权限链接中的机器人可使用功能的描述信息
  */
 @ApiModel
 @Serializable
-public data class ApiPermissionDemand @ApiModelConstructor constructor(
-    /**
-     * 申请接口权限的频道 id
-     */
-    @SerialName("guild_id") val guildId: String,
-    /**
-     * 接口权限需求授权链接发送的子频道 id
-     */
-    @SerialName("channel_id") val channelId: String,
-    /**
-     * 权限接口唯一标识
-     */
-    @SerialName("api_identify") val apiIdentify: ApiPermissionDemandIdentify,
+public class ApiPermissionDemand @ApiModelConstructor constructor(
+    @SerialName("guild_id") public val guildId: String,
+    @SerialName("channel_id") public val channelId: String,
+    @SerialName("api_identify") public val apiIdentify: ApiPermissionDemandIdentify,
+    public val title: String,
+    public val desc: String,
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ApiPermissionDemand) return false
 
-    /**
-     * 接口权限链接中的接口权限描述信息
-     */
-    val title: String,
-    /**
-     * 接口权限链接中的机器人可使用功能的描述信息
-     */
-    val desc: String,
-)
+        if (guildId != other.guildId) return false
+        if (channelId != other.channelId) return false
+        if (apiIdentify != other.apiIdentify) return false
+        if (title != other.title) return false
+        if (desc != other.desc) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = guildId.hashCode()
+        result = 31 * result + channelId.hashCode()
+        result = 31 * result + apiIdentify.hashCode()
+        result = 31 * result + title.hashCode()
+        result = 31 * result + desc.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "ApiPermissionDemand(" +
+            "guildId='$guildId', " +
+            "channelId='$channelId', " +
+            "apiIdentify=$apiIdentify, " +
+            "title='$title', " +
+            "desc='$desc')"
+    }
+
+    //region data-class 兼容
+    @Deprecated(DataClassCompatibilities.DEPRECATED_MESSAGE, ReplaceWith("guildId"))
+    public operator fun component1(): String = guildId
+
+    @Deprecated(DataClassCompatibilities.DEPRECATED_MESSAGE, ReplaceWith("channelId"))
+    public operator fun component2(): String = channelId
+
+    @Deprecated(DataClassCompatibilities.DEPRECATED_MESSAGE, ReplaceWith("apiIdentify"))
+    public operator fun component3(): ApiPermissionDemandIdentify = apiIdentify
+
+    @Deprecated(DataClassCompatibilities.DEPRECATED_MESSAGE, ReplaceWith("title"))
+    public operator fun component4(): String = title
+
+    @Deprecated(DataClassCompatibilities.DEPRECATED_MESSAGE, ReplaceWith("desc"))
+    public operator fun component5(): String = desc
+
+    @Suppress("DeprecatedCallableAddReplaceWith")
+    @Deprecated(DataClassCompatibilities.DEPRECATED_MESSAGE)
+    public fun copy(
+        guildId: String = this.guildId,
+        channelId: String = this.channelId,
+        apiIdentify: ApiPermissionDemandIdentify = this.apiIdentify,
+        title: String = this.title,
+        desc: String = this.desc,
+    ): ApiPermissionDemand = ApiPermissionDemand(guildId, channelId, apiIdentify, title, desc)
+    //endregion
+}
 
 /**
  * [接口权限需求标识对象（APIPermissionDemandIdentify）](https://bot.q.qq.com/wiki/develop/api/openapi/api_permissions/model.html#%E6%8E%A5%E5%8F%A3%E6%9D%83%E9%99%90%E9%9C%80%E6%B1%82%E6%A0%87%E8%AF%86%E5%AF%B9%E8%B1%A1-apipermissiondemandidentify)
+ *
+ * @property path API 接口名，例如 `/guilds/{guild_id}/members/{user_id}`
+ * @property method 请求方法，例如 `GET`
  */
 @Serializable
-public data class ApiPermissionDemandIdentify(
-    /**
-     * API 接口名，例如 `/guilds/{guild_id}/members/{user_id}`
-     */
-    val path: String,
-
-    /**
-     * 请求方法，例如 `GET`
-     */
-    val method: String,
+public class ApiPermissionDemandIdentify @ApiModelConstructor constructor(
+    public val path: String,
+    public val method: String,
 ) {
     public companion object {
+        /**
+         * 构建 [ApiPermissionDemandIdentify]。
+         *
+         * @since 5.0
+         */
+        @JvmStatic
+        public fun of(path: String, method: String): ApiPermissionDemandIdentify =
+            ApiPermissionDemandIdentify(path, method)
+
         /**
          * 通过 [ApiPermission] 构建一个 [ApiPermissionDemandIdentify].
          */
@@ -124,4 +218,39 @@ public data class ApiPermissionDemandIdentify(
         public fun ApiDescription.toIdentify(): ApiPermissionDemandIdentify =
             ApiPermissionDemandIdentify(path, method)
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ApiPermissionDemandIdentify) return false
+
+        if (path != other.path) return false
+        if (method != other.method) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = path.hashCode()
+        result = 31 * result + method.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "ApiPermissionDemandIdentify(path='$path', method='$method')"
+    }
+
+    //region data-class 兼容
+    @Deprecated(DataClassCompatibilities.DEPRECATED_MESSAGE, ReplaceWith("path"))
+    public operator fun component1(): String = path
+
+    @Deprecated(DataClassCompatibilities.DEPRECATED_MESSAGE, ReplaceWith("method"))
+    public operator fun component2(): String = method
+
+    @Suppress("DeprecatedCallableAddReplaceWith")
+    @Deprecated(DataClassCompatibilities.DEPRECATED_MESSAGE)
+    public fun copy(
+        path: String = this.path,
+        method: String = this.method,
+    ): ApiPermissionDemandIdentify = ApiPermissionDemandIdentify(path, method)
+    //endregion
 }
